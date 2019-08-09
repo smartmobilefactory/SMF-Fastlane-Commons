736694d50a5d16af00c06b7a960d30311015a126
Tim Depping <tim-lorenz@depping.org>
2019-08-09 12:15:09 +0200

    debugging

--- commons/smf_send_message/smf_send_notification.rb
+++ commons/smf_send_message/smf_send_notification.rb
@@ -2,7 +2,6 @@ desc "Post to a Slack room if the build was successful"
 private_lane :smf_send_app_build_success_notification do |options|
 
   build_variant = options[:build_variant]
-  project_name = !@smf_fastlane_config[:project][:project_name].nil? ? @smf_fastlane_config[:project][:project_name] : ENV["PROJECT_NAME"]
   slack_channel = @smf_fastlane_config[:project][:slack_channel]
 
   # Collect the changelog (again) in case the build job failed before the former changelog collecting
@@ -12,8 +11,10 @@ private_lane :smf_send_app_build_success_notification do |options|
   when :ios
     build_number = get_build_number(xcodeproj: "#{project_name}.xcodeproj")
     version = smf_get_version_number
+    project_name = @smf_fastlane_config[:project][:project_name]
     name = "#{project_name} #{build_variant.upcase} #{version} (#{build_number})"
   when :android
+    project_name = !@smf_fastlane_config[:project][:name].nil? ? @smf_fastlane_config[:project][:name] : ENV["PROJECT_NAME"]
     name = "#{project_name} #{build_variant} (Build #{ENV["next_version_code"]})"
   when :flutter
     UI.message('Notification for flutter is not implemented yet')
