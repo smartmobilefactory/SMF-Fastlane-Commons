b4a08f60154f7b5b999bb04fe9e4eef92cf5071e
Tim Depping <tim-lorenz@depping.org>
2019-08-20 10:00:57 +0200

    debugging

--- commons/smf_upload_to_appcenter/smf_upload_to_appcenter.rb
+++ commons/smf_upload_to_appcenter/smf_upload_to_appcenter.rb
@@ -10,13 +10,15 @@ private_lane :smf_upload_to_appcenter do |options|
   uri = URI.parse('https://api.appcenter.ms/v0.1/apps')
   request = Net::HTTP::Get.new(uri.request_uri)
   request['accept'] = 'application/json'
-  UI.message(ENV[$SMF_APPCENTER_API_TOKEN_ENV_KEY])
   request['X-API-Token'] = ENV[$SMF_APPCENTER_API_TOKEN_ENV_KEY]
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
+  t1 = Time.now
   response = http.request(request)
+  t2 = Time.now
+  UI.message("Request took #{t2 - t1} seconds.")
   if response.code != 200
-    raise("An error occured while fetching apps from AppCenter.")
+    raise("An error occured while fetching apps from AppCenter: #{response.message}")
   end
   UI.message(response.body)
   data = JSON.parse(response.body)
