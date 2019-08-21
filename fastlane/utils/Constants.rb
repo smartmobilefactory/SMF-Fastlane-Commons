ee193e4b14eb846056f068c5ff8a787e51dc5399
UrsKahmann <urs@smartmobilefactory.com>
2019-08-21 15:43:45 +0200

    Added two new constants which were hardcoded before

--- fastlane/utils/Constants.rb
+++ fastlane/utils/Constants.rb
@@ -23,6 +23,8 @@ $WORKSPACE_ENV_KEY = 'WORKSPACE'
 
 $SMF_IS_SLACK_ENABLED = 'SMF_IS_SLACK_ENABLED'
 $SMF_IS_KEYCHAIN_ENABLED = 'SMF_IS_KEYCHAIN_ENABLED'
+$KEYCHAIN_LOGIN_ENV_KEY = 'LOGIN'
+$KEYCHAIN_JENKINS_ENV_KEY = 'JENKINS'
 
 # Path to xcode versions, used for xcode_select and ensure_xcode_version in combination with the projects xcode version
 $XCODE_EXECUTABLE_PATH_PREFIX = '/Applications/Xcode-'
