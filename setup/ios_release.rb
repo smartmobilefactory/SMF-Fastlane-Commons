8124f0b6e281c8ee662a486180f73b9aa72ec668
UrsKahmann <urs@smartmobilefactory.com>
2019-08-30 15:00:18 +0200

    Removed before all black and put setup functionallity into the smf_common_setup lane

---
+++ setup/ios_release.rb
@@ -0,0 +1,17 @@
+# Setup Workspace (clean, clone, generate)
+
+lane :workspace_setup
+  UI.message("WORKSPACE SETUP")
+end
+# Setup Dependencies - pod install & `sh generate.sh` (optional: Phrase App)
+# Provisioning
+# run unittests (build test target and run test)
+# increment_buildnumber
+# build (build to release)
+# changelog
+# Upload Dsym
+# Upload Appcenter
+# Upload iTunes
+# Push git tag / Release
+# Slack
+# Monitoring (MetaJSON)
