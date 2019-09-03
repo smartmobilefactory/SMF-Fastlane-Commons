68dd0b80b89841a7a50f5245a324bc7c157fafee
Tim Depping <tim-lorenz@depping.org>
2019-09-03 15:16:36 +0200

    call pull keystore only if keystore folder is not null

--- commons/smf_build_app/smf_build_android_app.rb
+++ commons/smf_build_app/smf_build_android_app.rb
@@ -10,14 +10,17 @@ private_lane :smf_build_android_app do |options|
     UI.important("Building variant " + build_variant)
   end
 
-  keystore_values = smf_pull_keystore(folder: keystore_folder) if !keystore_folder.nil?
+  addition = ''
 
-  addition = ""
-  if keystore_values[:keystore_file]
-    addition = " -Pandroid.injected.signing.store.file='#{keystore_values[:keystore_file]}'"
-    addition << " -Pandroid.injected.signing.store.password='#{keystore_values[:keystore_password]}'"
-    addition << " -Pandroid.injected.signing.key.alias='#{keystore_values[:keystore_key_alias]}'"
-    addition << " -Pandroid.injected.signing.key.password='#{keystore_values[:keystore_key_password]}'"
+  unless keystore_folder.nil?
+    keystore_values = smf_pull_keystore(folder: keystore_folder)
+
+    if keystore_values[:keystore_file]
+      addition = " -Pandroid.injected.signing.store.file='#{keystore_values[:keystore_file]}'"
+      addition << " -Pandroid.injected.signing.store.password='#{keystore_values[:keystore_password]}'"
+      addition << " -Pandroid.injected.signing.key.alias='#{keystore_values[:keystore_key_alias]}'"
+      addition << " -Pandroid.injected.signing.key.password='#{keystore_values[:keystore_key_password]}'"
+    end
   end
 
   gradle(task: "assemble" + build_variant + addition)
