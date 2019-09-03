c7ecde3e3384d209cdbe14a3b08d683c41da5a9f
UrsKahmann <33418462+UrsKahmann@users.noreply.github.com>
2019-09-03 11:19:33 +0200

    SMFCI-537: Pull Keystore (#24)
    * Added smf_pull_keystore code to commons commons and removed it from the legacy code
    
    * Moved the "addition" string creation into the smf_pull_keystore lane.
    
    * Added keystore_folder paramtert and moved smf_pull_keystore call into the smf_build_android_app lane
    
    * checking if keystore folder path is passed, if not just skip the pull keystore part
    
    * changed logic to only pull_keystores when the keystore_folder path is not nil
    
    * Added documentation
    
    * Passing keystore values as map. Removed unused env variable keys.
    
    * Removed typos and simplefied the code

--- legacy/android/fastlane/lanes/Commons.rb
+++ legacy/android/fastlane/lanes/Commons.rb
@@ -1,23 +1,5 @@
 fastlane_require 'json'
 
-def load_properties(properties_filename)
-  properties = {}
-  File.open(properties_filename, 'r') do |properties_file|
-    properties_file.read.each_line do |line|
-      line.strip!
-      if (line[0] != ?# and line[0] != ?=)
-        i = line.index('=')
-        if (i)
-          properties[line[0..i - 1].strip] = line[i + 1..-1].strip
-        else
-          properties[line] = ''
-        end
-      end
-    end
-  end
-  properties
-end
-
 def load_config()
   load_json("../Config.json")
 end
