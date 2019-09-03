9e0ae414904b017dc9f1c54c9ab313bab6a04946
Tim Depping <tim-lorenz@depping.org>
2019-09-03 15:22:54 +0200

    debugging

--- fastlane/utils/utils.rb
+++ fastlane/utils/utils.rb
@@ -1,5 +1,6 @@
 def get_apk_path(apk_file_regex)
   Dir["/**/#{apk_file_regex}"].each do |file|
+    UI.message("Found apk at: #{File.path(file)}")
     path = File.path(file)
     break
   end
@@ -8,6 +9,7 @@ end
 def get_apk_file_regex(build_variant)
   variant = get_build_variant_from_config(build_variant)
   file_regex = "*-#{variant.gsub(/[A-Z]/).map(&:downcase).join('-')}.apk"
+  UI.message("Apk File Regex: #{file_regex}")
 end
 
 def get_build_variant_from_config(build_variant)
