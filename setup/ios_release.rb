d6bd376cb9e98cbaffdf8654675c027b4781599f
UrsKahmann <urs@smartmobilefactory.com>
2019-09-04 11:11:13 +0200

    debugging

--- setup/ios_release.rb
+++ setup/ios_release.rb
@@ -2,10 +2,10 @@
 private_lane :super_setup_dependencies do |options|
   UI.message("Reading Config.json at #{smf_workspace_dir}/Config.json")
   sh("cd #{smf_workspace_dir}; ls -a")
-  @smf_fastlane_config = JSON.parse(File.read("#{smf_workspace_dir}/Config.json"))
+  phrase_app_properties = JSON.parse(File.read("#{smf_workspace_dir}/Config.json"), :symbolize_names => true)[options[:build_variant].to_sym][:phrase_app]
   smf_pod_install
-  UI.message("properties: #{get_phrase_app_properties}")
-  smf_sync_with_phrase_app(get_phrase_app_properties)
+  UI.message("properties: #{phrase_app_properties}")
+  smf_sync_with_phrase_app(phrase_app_properties)
 end
 
 lane :setup_dependencies do |options|
