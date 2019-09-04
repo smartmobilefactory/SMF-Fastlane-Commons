8af61b2cc6364fce08d411da74000fb2ef37d567
Tim Depping <tim-lorenz@depping.org>
2019-09-04 13:12:18 +0200

    debugging

--- fastlane/utils/utils.rb
+++ fastlane/utils/utils.rb
@@ -56,3 +56,25 @@ def get_default_name_of_app(build_variant)
     raise 'Unknown platform'
   end
 end
+
+def get_build_number_of_app
+  UI.message('Get the build number of project.')
+  case @platform
+  when :ios
+    project_name = @smf_fastlane_config[:project][:project_name]
+    build_number = get_build_number(xcodeproj: "#{project_name}.xcodeproj")
+  when :android
+    build_number = @smf_fastlane_config['app_version_code'].to_s
+  when :flutter
+    UI.message('get build number of project for flutter is not implemented yet')
+  else
+    UI.message("There is no platform \"#{@platform}\", exiting...")
+    raise 'Unknown platform'
+  end
+
+  if build_number.include? '.'
+    parts = build_number.split('.')
+    build_number = parts[0]
+  end
+  build_number
+end
