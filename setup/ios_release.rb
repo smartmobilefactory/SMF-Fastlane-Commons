b49496e8baec459e9cfa195d68f47a13cf5c92a9
UrsKahmann <urs@smartmobilefactory.com>
2019-09-04 11:33:59 +0200

    finished setup_dependencies lane

--- setup/ios_release.rb
+++ setup/ios_release.rb
@@ -1,14 +1,12 @@
 # Setup Dependencies - pod install & `sh generate.sh` (optional: Phrase App)
 private_lane :super_setup_dependencies do |options|
-  UI.message("Reading Config.json at #{smf_workspace_dir}/Config.json")
-  sh("cd #{smf_workspace_dir}; ls -a")
-  UI.message("Build varinat: #{options[:build_variant]}")
-  config = JSON.parse(File.read("#{smf_workspace_dir}/Config.json"), :symbolize_names => true)
-  build_variant = options[:build_variant].to_sym
-  phrase_app_properties = config[:build_variants][build_variant]#[:phrase_app]
-  UI.message("properties: #{phrase_app_properties}")
+
   smf_pod_install
-  UI.message("properties: #{phrase_app_properties}")
+
+  UI.message("Reading Config.json at #{smf_workspace_dir}/Config.json")
+  config = JSON.parse(File.read("#{smf_workspace_dir}/Config.json"), :symbolize_names => true)
+  phrase_app_properties = config[:build_variants][options[:build_variant].to_sym][:phrase_app]
+
   smf_sync_with_phrase_app(phrase_app_properties)
 end
 
