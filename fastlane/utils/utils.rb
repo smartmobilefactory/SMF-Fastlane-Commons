a0780de8fcf8c29cc6861b2ada11eb509cab33e9
Tim Depping <tim-lorenz@depping.org>
2019-09-04 11:25:33 +0200

    debugging

--- fastlane/utils/utils.rb
+++ fastlane/utils/utils.rb
@@ -13,6 +13,7 @@ def get_apk_file_regex(build_variant)
   variant = get_build_variant_from_config(build_variant)
   file_regex = "*-#{variant.gsub(/[A-Z]/) { |s| '-' + s.downcase }}.apk"
   UI.message("Apk File Regex: #{file_regex}")
+  file_regex
 end
 
 def get_build_variant_from_config(build_variant)
