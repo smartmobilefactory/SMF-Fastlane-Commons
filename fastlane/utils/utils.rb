ba5f2c5a089383cb2ac84620ecfa512d09e8038c
Tim Depping <tim-lorenz@depping.org>
2019-09-04 11:21:00 +0200

    debugging

--- fastlane/utils/utils.rb
+++ fastlane/utils/utils.rb
@@ -1,5 +1,6 @@
 def get_apk_path(apk_file_regex)
   search_path = "#{smf_workspace_dir}/**/#{apk_file_regex}"
+  UI.message(search_path)
   Dir[search_path.to_s].each do |file|
     path = File.expand_path(file)
     UI.message("Found apk at: #{path}")
