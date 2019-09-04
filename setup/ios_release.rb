ca50fbc6c840113e7d31020029a8fab2f15ce2a0
UrsKahmann <urs@smartmobilefactory.com>
2019-09-04 11:22:49 +0200

    debugging

--- setup/ios_release.rb
+++ setup/ios_release.rb
@@ -2,6 +2,7 @@
 private_lane :super_setup_dependencies do |options|
   UI.message("Reading Config.json at #{smf_workspace_dir}/Config.json")
   sh("cd #{smf_workspace_dir}; ls -a")
+  UI.message("Build varinat: #{options[:build_variant]}")
   phrase_app_properties = JSON.parse(File.read("#{smf_workspace_dir}/Config.json"), :symbolize_names => true)#[options[:build_variant].to_sym][:phrase_app]
   UI.message("properties: #{phrase_app_properties}")
   smf_pod_install
