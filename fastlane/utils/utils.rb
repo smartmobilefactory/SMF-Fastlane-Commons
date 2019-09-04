7f16968ef7fc42bbf157d51ecdf1eb468c0a26c9
Tim Depping <tim-lorenz@depping.org>
2019-09-04 14:29:37 +0200

    minor changes

--- fastlane/utils/utils.rb
+++ fastlane/utils/utils.rb
@@ -11,15 +11,11 @@ end
 def get_apk_file_regex(build_variant)
   variant = get_build_variant_from_config(build_variant)
   file_regex = "*-#{variant.gsub(/[A-Z]/) { |s| '-' + s.downcase }}.apk"
-  UI.message("Apk File Regex: #{file_regex}")
-  file_regex
 end
 
 def get_build_variant_from_config(build_variant)
   build_variant = build_variant.to_s.downcase
   variant = @smf_fastlane_config[:build_variants][build_variant.to_sym][:variant]
-
-  variant
 end
 
 def get_project_name
