65209fa7d7fdaa1e1898f31a0f7c5ac9a257b8ad
UrsKahmann <urs@smartmobilefactory.com>
2019-09-04 10:55:06 +0200

    reading the fastlane config in the beginning

--- setup/ios_release.rb
+++ setup/ios_release.rb
@@ -1,6 +1,8 @@
 # Setup Dependencies - pod install & `sh generate.sh` (optional: Phrase App)
 private_lane :super_setup_dependencies do |options|
+  @smf_fastlane_config = JSON.parse(File.read('#{smf_workspace_dir}"/Config.json'))
   smf_pod_install
+  UI.message("properties: #{get_phrase_app_properties}")
   smf_sync_with_phrase_app(get_phrase_app_properties)
 end
 
