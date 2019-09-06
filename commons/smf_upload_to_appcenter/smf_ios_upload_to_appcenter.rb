7ec1cfa110dace07678393832b076b3300b7b9f8
UrsKahmann <urs@smartmobilefactory.com>
2019-09-06 14:56:49 +0200

    debugging

--- commons/smf_upload_to_appcenter/smf_ios_upload_to_appcenter.rb
+++ commons/smf_upload_to_appcenter/smf_ios_upload_to_appcenter.rb
@@ -19,7 +19,7 @@ private_lane :smf_ios_upload_to_appcenter do |options|
   NO_APP_FAILURE = 'NO_APP_FAILURE'
 
   unless is_mac_app
-    sh "cd ../build; zip -r9 \"#{escaped_filename}.ipa.zip\" \"#{escaped_filename}.app\" || echo #{NO_APP_FAILURE}"
+    sh "cd ../build; zip -r9 \"#{escaped_filename}.ipa.zip\" \"#{escaped_filename}.ipa\" || echo #{NO_APP_FAILURE}"
   end
 
   app_path = path_to_ipa_or_app
