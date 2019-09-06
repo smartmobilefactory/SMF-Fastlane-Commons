a35ce6d9f33dfb1086d27dba5ba47effceccc6c4
UrsKahmann <urs@smartmobilefactory.com>
2019-09-06 14:33:30 +0200

    debugging

--- commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
+++ commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
@@ -13,23 +13,3 @@ private_lane :smf_create_dmg_from_app do |options|
   # Create the dmg with the script and store it in the same directory as the app
   sh "##{@fastlane_commons_dir_path}/commons/ios/smf_create_dmg_from_app/create_dmg.sh -p #{app_path} -ci #{team_id}"
 end
-
-def smf_path_to_ipa_or_app(build_scheme)
-
-  escaped_filename = build_scheme.gsub(" ", "\ ")
-
-  app_path = Pathname.getwd.dirname.to_s + "/build/#{escaped_filename}.ipa.zip"
-  if ( ! File.exists?(app_path))
-    app_path =  Pathname.getwd.dirname.to_s + "/build/#{escaped_filename}.ipa"
-  end
-
-  UI.message("Constructed path \"#{app_path}\" from filename \"#{escaped_filename}\"")
-
-  unless File.exist?(app_path)
-    app_path = lane_context[SharedValues::IPA_OUTPUT_PATH]
-
-    UI.message("Using \"#{app_path}\" as app_path as no file exists at the constructed path.")
-  end
-
-  return app_path
-end
--- fastlane/utils/utils.rb
+++ fastlane/utils/utils.rb
@@ -76,12 +76,12 @@ def smf_get_app_secret(build_variant)
   end
 end
 
-def get_path_to_ipa_or_app(build_variant)
+def smf_path_to_ipa_or_app(build_variant)
 
   escaped_filename = @smf_fastlane_config[:build_variants][build_variant.to_sym][:scheme].gsub(' ', "\ ")
 
-  app_path = Pathname.getwd.dirname.to_s + "/build/#{escaped_filename}.app.zip"
-  app_path = Pathname.getwd.dirname.to_s + "/build/#{escaped_filename}.app" unless File.exist?(app_path)
+  app_path = Pathname.getwd.dirname.to_s + "/build/#{escaped_filename}.ipa.zip"
+  app_path = Pathname.getwd.dirname.to_s + "/build/#{escaped_filename}.ipa" unless File.exist?(app_path)
 
   UI.message("Constructed path \"#{app_path}\" from filename \"#{escaped_filename}\"")
 
