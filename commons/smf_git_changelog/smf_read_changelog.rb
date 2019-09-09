70b393cf3d200d962a3783b6d786e79f8e1cb1c1
UrsKahmann <urs@smartmobilefactory.com>
2019-09-09 12:08:47 +0200

    debugging

--- commons/smf_git_changelog/smf_read_changelog.rb
+++ commons/smf_git_changelog/smf_read_changelog.rb
@@ -1,5 +1,5 @@
 private_lane :smf_read_changelog do |options|
-  UI.message("REading changelog from #{_smf_changelog_temp_path}")
+  UI.message("Reading changelog from #{_smf_changelog_temp_path}")
   changelog = File.read(_smf_changelog_temp_path)
 
   if options[:remove_changelog] == true
