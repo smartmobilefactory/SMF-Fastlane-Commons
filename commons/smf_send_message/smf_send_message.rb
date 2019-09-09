a7c15f42e30c1cb34092aff8f9d9898882e311ef
UrsKahmann <urs@smartmobilefactory.com>
2019-09-09 15:00:21 +0200

    removed unused constant

--- commons/smf_send_message/smf_send_message.rb
+++ commons/smf_send_message/smf_send_message.rb
@@ -1,9 +1,6 @@
 desc 'Sending a message to the given Slack channel'
 private_lane :smf_send_message do |options|
 
-  # Skip sending if slack is disabled
-  return unless smf_is_slack_enabled
-
   slack_workspace_url = "https://hooks.slack.com/services/#{ENV[$SMF_SLACK_URL]}"
   title = "*#{options[:title]}*"
   message = !options[:message].nil? ? options[:message] : ''
