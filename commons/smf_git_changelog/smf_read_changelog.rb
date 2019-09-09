6dbea62490255f49f9950c8300c3c29b09cdb776
UrsKahmann <urs@smartmobilefactory.com>
2019-09-09 12:04:46 +0200

    debugging

--- commons/smf_git_changelog/smf_read_changelog.rb
+++ commons/smf_git_changelog/smf_read_changelog.rb
@@ -1,10 +1,10 @@
 private_lane :smf_read_changelog do |options|
   UI.message("REading changelog from #{_smf_changelog_temp_path}")
-  changelog = File.read(smf_changelog_temp_path)
+  changelog = File.read(_smf_changelog_temp_path)
 
   if options[:remove_changelog] == true
     UI.message("Removing temporary changelog file at #{_smf_changelog_temp_path}")
-    sh "rm #{smf_changelog_temp_path}" if File.exist?(smf_changelog_temp_path)
+    sh "rm #{_smf_changelog_temp_path}" if File.exist?(_smf_changelog_temp_path)
   end
 
   changelog.to_s
