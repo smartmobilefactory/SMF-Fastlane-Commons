1285833ff8b89635d47d07c592ffef7a7c4e4f2a
UrsKahmann <urs@smartmobilefactory.com>
2019-09-11 13:38:16 +0200

    added lane to generate files (jenkins file for now)

---
+++ commons/smf_update_generated_files/smf_update_generated_files.rb
@@ -0,0 +1,29 @@
+desc "Generates a Jenkinsfile and commits it if there are changes"
+private_lane :smf_update_generated_files do |options|
+
+    smf_generate_jenkins_file
+
+    jenkinsfile_changed = false
+    Dir.chdir(smf_workspace_dir) do
+      jenkinsfile_changed = `git status --porcelain`.include? 'Jenkinsfile'
+    end
+
+    UI.message('Checking for Jenkinsfile changes...')
+
+    # If something changed in config
+    if jenkinsfile_changed
+      UI.message('Jenkinsfile changed since last build, will synchronize and commit the changes...')
+
+      git_add(path: './Jenkinsfile')
+      git_commit(path: '.', message: 'Updated Generated Jenkinsfile')
+
+      unlock_keychain(path: 'login.keychain', password: ENV['LOGIN'])
+      unlock_keychain(path: 'jenkins.keychain', password: ENV['JENKINS'])
+
+      smf_push_to_git_remote(remote_branch: options[:branch])
+
+      UI.user_error!('Generated Jenkinsfile changed since last build, build will be restarted. This is not a failure.')
+    else
+      UI.success('Generated Jenkinsfile is up to date. Nothing to do.')
+    end
+  end
