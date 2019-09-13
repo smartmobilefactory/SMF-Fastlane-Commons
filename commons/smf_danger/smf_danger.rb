4a84baf17fb16d286771b2ca4600481aa01ad280
UrsKahmann <urs@smartmobilefactory.com>
2019-09-13 10:50:53 +0200

    using the same code for android and ios

--- commons/smf_danger/smf_danger.rb
+++ commons/smf_danger/smf_danger.rb
@@ -1,29 +1,26 @@
 private_lane :smf_danger do |options|
 
   checkstyle_paths = []
-  case @platform
-  when :ios
-    if File.exist?(smf_swift_lint_output_path)
-      checkstyle_paths.push(smf_swift_lint_output_path)
-    else
-      UI.warning("There is not SwiftLint output file at #{smf_swift_lint_output_path}. Is SwiftLint enabled?")
-    end
-  when :android
-    jira_keys = options[:jira_key]
 
-    UI.user_error!("android-commons not present! Can't start danger") unless File.exist?('../android-commons')
-
-    lint_paths = _smf_find_paths_of('lint-result.xml')
-    junit_result_paths = _smf_find_paths_of_files_in_directory('build/test-results', 'xml')
-
-    _smf_find_paths_of('klint.xml').each { |path| checkstyle_paths.push(path) }
-    _smf_find_paths_of('detekt.xml').each { |path| checkstyle_paths.push(path) }
-
-    ENV["DANGER_JIRA_KEYS"] = JSON.dump(_smf_danger_jira_key_parameter(jira_keys))
-    ENV["DANGER_LINT_PATHS"] = JSON.dump(lint_paths)
-    ENV["DANGER_JUNIT_PATHS"] = JSON.dump(junit_result_paths)
+  if File.exist?(smf_swift_lint_output_path)
+    checkstyle_paths.push(smf_swift_lint_output_path)
+  else if @platform == :ios
+    UI.warning("There is not SwiftLint output file at #{smf_swift_lint_output_path}. Is SwiftLint enabled?")
   end
 
+  jira_keys = options[:jira_key]
+
+  UI.user_error!("android-commons not present! Can't start danger") unless File.exist?('../android-commons')
+
+  lint_paths = _smf_find_paths_of('lint-result.xml')
+  junit_result_paths = _smf_find_paths_of_files_in_directory('build/test-results', 'xml')
+
+  _smf_find_paths_of('klint.xml').each { |path| checkstyle_paths.push(path) }
+  _smf_find_paths_of('detekt.xml').each { |path| checkstyle_paths.push(path) }
+
+  ENV["DANGER_JIRA_KEYS"] = JSON.dump(_smf_danger_jira_key_parameter(jira_keys))
+  ENV["DANGER_LINT_PATHS"] = JSON.dump(lint_paths)
+  ENV["DANGER_JUNIT_PATHS"] = JSON.dump(junit_result_paths)
   ENV["DANGER_CHECKSTYLE_PATHS"] = JSON.dump(checkstyle_paths)
 
   danger(
