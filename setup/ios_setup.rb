b00854a74e161765ebdb12a82d245ac1847c221c
UrsKahmann <urs@smartmobilefactory.com>
2019-09-19 15:51:45 +0200

    debugging

--- setup/ios_setup.rb
+++ setup/ios_setup.rb
@@ -20,7 +20,7 @@ private_lane :smf_super_setup_dependencies do |options|
   )
 end
 
-lane :smf_setup_dependencies do |options|
+override_lane :smf_setup_dependencies do |options|
   smf_super_setup_dependencies(options)
 end
 
