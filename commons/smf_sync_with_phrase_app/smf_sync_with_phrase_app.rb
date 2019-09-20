ffde3f932fe318b3d6e9c2fbf9e0f05bbde67130
UrsKahmann <urs@smartmobilefactory.com>
2019-09-20 12:02:51 +0200

    Passing projects git branch as default branch to the phraseapp lane

--- commons/smf_sync_with_phrase_app/smf_sync_with_phrase_app.rb
+++ commons/smf_sync_with_phrase_app/smf_sync_with_phrase_app.rb
@@ -1,7 +1,8 @@
 lane :smf_sync_with_phrase_app do |options|
   case @platform
   when :ios
-    initialize_env_variable_name_mappings
+    default_git_branch = options[:git_branch]
+    initialize_env_variable_name_mappings(default_git_branch)
     UI.message("Strings are synced with PhraseApp using the values from the fastlane/Config.json")
 
     if (!validate_and_set_phrase_app_env_variables(options))
@@ -50,7 +51,7 @@ end
 # the boolean value indicates whether the value is optional or not
 # for default values a third entry in the array can be provided
 
-def initialize_env_variable_name_mappings
+def initialize_env_variable_name_mappings(git_branch)
   @phrase_app_config_keys_env_variable_mapping = {
       :access_token_key           => ["phraseappAccessToken", true, $SMF_PHRASE_APP_ACCESS_TOKEN_KEY], # optional
       :project_id                 => ["phraseappProjectId", false],
@@ -59,7 +60,7 @@ def initialize_env_variable_name_mappings
       :format                     => ["phraseappFormat", false],
       :base_directory             => ["phraseappBasedir", false],
       :files                      => ["phraseappFiles", false],
-      :git_branch                 => ["phraseappGitBranch", true, @smf_git_branch],  # optional, defaults to @smf_git_branch
+      :git_branch                 => ["phraseappGitBranch", true, git_branch],  # optional, defaults to @smf_git_branch
       :files_prefix               => ["phraseappFilesPrefix", true, ""], # optional
       :forbid_comments_in_source  => ["phraseappForbidCommentsInSource", true, "1"]  # optional
   }
