5965d56b1b78361753f099e2bf6bc1a966221661
Tim Depping <tim-lorenz@depping.org>
2019-09-24 15:08:22 +0200

    use constants to find keystore properties refactor smf pull keystore lane

--- commons/android/smf_pull_keystore/smf_pull_keystore.rb
+++ commons/android/smf_pull_keystore/smf_pull_keystore.rb
@@ -1,22 +1,21 @@
-
 private_lane :smf_pull_keystore do |options|
-  
+
   clone_root_folder = !options[:clone_root_folder].nil? ? options[:clone_root_folder] : @fastlane_commons_dir_path
 
-  keystoreFolder = options[:folder]
+  keystore_folder = options[:folder]
 
   Dir.chdir(clone_root_folder) do
     sh("rm -r -f ./Android-Keystores")
     sh("git clone https://github.com/smartmobilefactory/Android-Keystores.git")
-    sh("cd ./Android-Keystores; sh crypto.sh -decrypt #{keystoreFolder}")
+    sh("cd ./Android-Keystores; sh crypto.sh -decrypt #{keystore_folder}")
   end
 
-  properties = load_properties("#{clone_root_folder}/Android-Keystores/keystores/#{keystoreFolder}/keystore.properties")
+  properties = load_properties("#{clone_root_folder}/Android-Keystores/keystores/#{keystore_folder}/keystore.properties")
   keystore_values = {}
-  keystore_values[:keystore_file] = File.absolute_path("#{clone_root_folder}/Android-Keystores/keystores/#{keystoreFolder}/keystore.jks")
-  keystore_values[:keystore_password] = properties[$SMF_KEYSTORE_PASSWORD_KEY]
-  keystore_values[:keystore_key_alias]  = properties[$SMF_KEYSTORE_KEY_ALIAS_KEY]
-  keystore_values[:keystore_key_password] = properties[$SMF_KEYSTORE_KEY_PASSWORD_KEY]
+  keystore_values[:keystore_file] = File.absolute_path("#{clone_root_folder}/Android-Keystores/keystores/#{keystore_folder}/keystore.jks")
+  keystore_values[:keystore_password] = properties['KEYSTORE_PASSWORD']
+  keystore_values[:keystore_key_alias] = properties['KEYSTORE_KEY_ALIAS']
+  keystore_values[:keystore_key_password] = properties['KEYSTORE_KEY_PASSWORD']
 
   keystore_values
 end
@@ -26,15 +25,16 @@ def load_properties(properties_filename)
   File.open(properties_filename, 'r') do |properties_file|
     properties_file.read.each_line do |line|
       line.strip!
-      if (line[0] != ?# and line[0] != ?=)
-        i = line.index('=')
-        if (i)
-          properties[line[0..i - 1].strip] = line[i + 1..-1].strip
-        else
-          properties[line] = ''
-        end
+      next if !(line[0] != ?# && line[0] != ?=)
+
+      i = line.index('=')
+      if i
+        properties[line[0..i - 1].strip] = line[i + 1..-1].strip
+      else
+        properties[line] = ''
       end
     end
   end
+
   properties
 end
