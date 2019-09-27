d51a64a3a2ce59820cd128dbb9ac340edb2d9510
Tim Depping <tim-lorenz@depping.org>
2019-09-27 15:47:29 +0200

    debugging

--- setup/flutter_setup.rb
+++ setup/flutter_setup.rb
@@ -68,7 +68,36 @@ end
 
 private_lane :smf_super_ios_build do |options|
   build_variant = !options[:build_variant].nil? ? options[:build_variant] : smf_get_first_variant_from_config
+  build_variant_config = @smf_fastlane_config[:build_variants][build_variant.to_sym]
+
   sh("cd #{smf_workspace_dir}; ./flutterw build ios --release --no-codesign --flavor #{build_variant}")
+
+  smf_download_provisioning_profiles(
+      team_id: build_variant_config[:team_id],
+      apple_id: build_variant_config[:apple_id],
+      use_wildcard_signing: build_variant_config[:use_wildcard_signing],
+      bundle_identifier: build_variant_config[:bundle_identifier],
+      use_default_match_config: build_variant_config[:match].nil?,
+      match_read_only: build_variant_config[:match].nil? ? nil : build_variant_config[:match][:read_only],
+      match_type: build_variant_config[:match].nil? ? nil : build_variant_config[:match][:type],
+      extensions_suffixes: @smf_fastlane_config[:extensions_suffixes],
+      build_variant: build_variant
+  )
+
+  smf_build_ios_app(
+      skip_export: options[:skip_export].nil? ? false : options[:skip_export],
+      scheme: build_variant_config[:scheme],
+      should_clean_project: build_variant_config[:should_clean_project],
+      required_xcode_version: @smf_fastlane_config[:project][:xcode_version],
+      project_name: @smf_fastlane_config[:project][:project_name],
+      xcconfig_name: smf_get_xcconfig_name(build_variant.to_sym),
+      code_signing_identity: build_variant_config[:code_signing_identity],
+      upload_itc: build_variant_config[:upload_itc].nil? ? false : build_variant_config[:upload_itc],
+      upload_bitcode: build_variant_config[:upload_bitcode].nil? ? true : build_variant_config[:upload_bitcode],
+      export_method: build_variant_config[:export_method],
+      icloud_environment: smf_get_icloud_environment(build_variant.to_sym)
+  )
+
 end
 
 lane :smf_ios_build do |options|
@@ -78,6 +107,8 @@ end
 private_lane :smf_super_android_build do |options|
   build_variant = !options[:build_variant].nil? ? options[:build_variant] : smf_get_first_variant_from_config
   sh("cd #{smf_workspace_dir}; ./flutterw build apk --release --flavor #{build_variant}")
+
+
 end
 
 lane :smf_android_build do |options|
@@ -161,7 +192,7 @@ private_lane :smf_super_pipeline_ios_upload_to_appcenter do |options|
       build_number: smf_get_build_number_of_app,
       app_id: appcenter_app_id,
       escaped_filename: build_variant_config[:flavor].gsub(' ', "\ "),
-      path_to_ipa_or_app: "#{smf_get_file_path(app_file_regex)}.ipa",
+      path_to_ipa_or_app: smf_get_file_path(app_file_regex),
       is_mac_app: build_variant_config[:use_sparkle],
       podspec_path: build_variant_config[:podspec_path]
   ) if !appcenter_app_id.nil?
@@ -171,7 +202,7 @@ private_lane :smf_super_pipeline_ios_upload_to_appcenter do |options|
       build_number: smf_get_build_number_of_app,
       app_id: hockey_app_id,
       escaped_filename: build_variant_config[:scheme].gsub(' ', "\ "),
-      path_to_ipa_or_app: "#{smf_get_file_path(app_file_regex)}.ipa",
+      path_to_ipa_or_app: smf_get_file_path(app_file_regex),
       is_mac_app: build_variant_config[:use_sparkle],
       podspec_path: build_variant_config[:podspec_path]
   ) if !hockey_app_id.nil?
