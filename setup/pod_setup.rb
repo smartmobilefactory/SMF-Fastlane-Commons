b4e70fbf6cebbb0cc69492057b18b0d70777ac07
UrsKahmann <urs@smartmobilefactory.com>
2019-10-15 12:43:52 +0200

    debugging

--- setup/pod_setup.rb
+++ setup/pod_setup.rb
@@ -51,3 +51,18 @@ end
 lane :smf_linter_for_pod_pr_check do |options|
   smf_super_linter_for_pod_pr_check(options)
 end
+
+private_lane :smf_super_danger_for_pod_pr_check do |options|
+
+  build_variants_for_pr_check = smf_build_variants_for_pod_pr_check
+  build_variants_for_pr_check.each { |variant|
+    UI.message("Running unit tests for variant '#{variant}' for PR Check")
+    options[:build_variant] = variant
+    smf_pipeline_danger(options)
+  }
+
+end
+
+lane :smf_danger_for_pod_pr_check do |options|
+  smf_super_danger_for_pod_pr_check(options)
+end
