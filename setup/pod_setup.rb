4822f4ec6c9105eb4e8c8957e1baf7b784e84d85
Tim Depping <tim-lorenz@depping.org>
2019-10-16 15:33:28 +0200

    add lanes for build process

--- setup/pod_setup.rb
+++ setup/pod_setup.rb
@@ -6,7 +6,9 @@ else
   raise "Can't find ios_setup file at #{ios_setup_file}"
 end
 
+
 # Build (Build to Release)
+
 private_lane :smf_super_build_for_pod_pr_check do |options|
 
   build_variants_for_pr_check = smf_build_variants_for_pod_pr_check
@@ -15,12 +17,14 @@ private_lane :smf_super_build_for_pod_pr_check do |options|
     options[:build_variant] = variant
     smf_build(options)
   }
-
 end
 
 lane :smf_build_for_pod_pr_check do |options|
   smf_super_build_for_pod_pr_check(options)
 end
+
+
+# Run Unit Tests
 
 private_lane :smf_super_unit_tests_for_pod_pr_check do |options|
 
@@ -30,12 +34,14 @@ private_lane :smf_super_unit_tests_for_pod_pr_check do |options|
     options[:build_variant] = variant
     smf_unit_tests(options)
   }
-
 end
 
 lane :smf_unit_tests_for_pod_pr_check do |options|
   smf_super_unit_tests_for_pod_pr_check(options)
 end
+
+
+# Linter
 
 private_lane :smf_super_linter_for_pod_pr_check do |options|
 
@@ -45,12 +51,14 @@ private_lane :smf_super_linter_for_pod_pr_check do |options|
     options[:build_variant] = variant
     smf_linter(options)
   }
-
 end
 
 lane :smf_linter_for_pod_pr_check do |options|
   smf_super_linter_for_pod_pr_check(options)
 end
+
+
+# Danger
 
 private_lane :smf_super_danger_for_pod_pr_check do |options|
 
@@ -60,9 +68,37 @@ private_lane :smf_super_danger_for_pod_pr_check do |options|
     options[:build_variant] = variant
     smf_pipeline_danger(options)
   }
-
 end
 
 lane :smf_danger_for_pod_pr_check do |options|
   smf_super_danger_for_pod_pr_check(options)
+end
+
+
+# Increment Version Number
+
+private_lane :smf_super_pipeline_increment_version_number do |options|
+
+  build_variant = options[:build_variant]
+  build_variant_config = @smf_fastlane_config[:build_variants][build_variant.to_sym]
+
+  smf_increment_version_number(
+      podspec_path: build_variant_config[:podspec_path],
+      bump_type: build_variant
+  )
+end
+
+lane :smf_pipeline_increment_version_number do |options|
+  smf_super_pipeline_increment_version_number(options)
+end
+
+
+# Generate Changelog
+
+private_lane :smf_super_generate_changelog do |options|
+  smf_git_changelog(is_library: true)
+end
+
+lane :smf_generate_changelog do |options|
+  smf_super_generate_changelog(options)
 end
