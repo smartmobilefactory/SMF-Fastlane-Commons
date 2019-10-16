61b4d94bd6cb33741a87b45901f54fa88f2e4b79
Tim Depping <tim-lorenz@depping.org>
2019-10-16 13:42:27 +0200

    debugging

--- commons/ios/smf_build_variants_for_pod_pr_check/smf_build_variants_for_pod_pr_check.rb
+++ commons/ios/smf_build_variants_for_pod_pr_check/smf_build_variants_for_pod_pr_check.rb
@@ -3,7 +3,7 @@ private_lane :smf_build_variants_for_pod_pr_check do
   all_build_variants = @smf_fastlane_config[:build_variants].keys.map(&:to_s)
 
   # Check for alpha in build variants.
-  alpha_build_variant = all_build_variants.detect { [variant].match(/.*alpha.*/) }
+  alpha_build_variant = all_build_variants.detect { |variant| variant.match(/.*alpha.*/) }
 
   # If there is an alpha return this alpha in an array. Otherwise return all build variants which contain 'example'.
   matching_build_variants = if alpha_build_variant.nil?
