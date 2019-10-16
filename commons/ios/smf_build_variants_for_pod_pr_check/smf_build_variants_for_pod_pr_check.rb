3a05a7bce35f5ab5806998d0ec5600e8879a95c1
Tim Depping <tim-lorenz@depping.org>
2019-10-16 14:07:48 +0200

    debugging

--- commons/ios/smf_build_variants_for_pod_pr_check/smf_build_variants_for_pod_pr_check.rb
+++ commons/ios/smf_build_variants_for_pod_pr_check/smf_build_variants_for_pod_pr_check.rb
@@ -8,7 +8,7 @@ private_lane :smf_build_variants_for_pod_pr_check do
   # If there is an alpha return this alpha in an array. Otherwise return all build variants which contain 'example'.
   matching_build_variants = if alpha_build_variant.nil?
                               all_build_variants.map do |variant|
-                                variant if variant.match(/.*example.*/)
+                                variant if !variant.nil? && variant.match(/.*example.*/)
                               end
                             else
                               [alpha_build_variant]
