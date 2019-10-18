0ce7b8d609b8b3d1afe0afb13b8c6f1b6c21fd29
UrsKahmann <urs@smartmobilefactory.com>
2019-10-18 16:15:01 +0200

    debugging

--- setup/ios_framework_setup.rb
+++ setup/ios_framework_setup.rb
@@ -117,6 +117,11 @@ private_lane :smf_super_release_pod do |options|
   specs_repo = build_variant_config[:pods_specs_repo]
   local_branch = options[:local_branch]
 
+  if smf_is_keychain_enabled
+    unlock_keychain(path: "login.keychain", password: ENV[$KEYCHAIN_LOGIN_ENV_KEY])
+    unlock_keychain(path: "jenkins.keychain", password: ENV[$KEYCHAIN_JENKINS_ENV_KEY])
+  end
+  
   smf_git_pull(local_branch)
 
   smf_push_pod(
@@ -131,11 +136,6 @@ private_lane :smf_super_release_pod do |options|
   )
 
   changelog = smf_read_changelog
-
-  if smf_is_keychain_enabled
-    unlock_keychain(path: "login.keychain", password: ENV[$KEYCHAIN_LOGIN_ENV_KEY])
-    unlock_keychain(path: "jenkins.keychain", password: ENV[$KEYCHAIN_JENKINS_ENV_KEY])
-  end
 
   # Create the GitHub release
   smf_create_github_release(
