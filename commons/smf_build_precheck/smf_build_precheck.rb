ac5cdcd256fad2ac197fe78a537941b38478ea39
kondeichmann <k.deichmann@mac.com>
2019-10-23 15:27:01 +0200

    Write message to Slack

--- commons/smf_build_precheck/smf_build_precheck.rb
+++ commons/smf_build_precheck/smf_build_precheck.rb
@@ -8,11 +8,16 @@ end
 def perform_build_precheck_ios(build_variant, build_variant_config)
 
 	if build_variant_config[:upload_itc] == true && build_variant_config[:itc_apple_id].nil?
-		raise ITCAppleIdError, "itc_apple_id not set in Config.json. Please Read: https://smartmobilefactory.atlassian.net/wiki/spaces/SMFIOS/pages/669646876/Missing+itc+apple+id "
+		message = "itc_apple_id not set in Config.json. Please Read: https://smartmobilefactory.atlassian.net/wiki/spaces/SMFIOS/pages/669646876/Missing+itc+apple+id "
+
+		smf_send_message(
+    		title: 'Build Precheck Error',
+    		message: message,
+    		type: 'error'
+    	)
+
+		raise message
 	else
 		UI.message("Build Precheck: Nothing reportable found")
 	end
-end
-
-class ITCAppleIdError < StandardError
 end
