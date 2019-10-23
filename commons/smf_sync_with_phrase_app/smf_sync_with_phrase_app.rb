c29f53962d1af3285744ca129d380e81d4e8c235
UrsKahmann <urs@smartmobilefactory.com>
2019-10-23 14:50:33 +0200

    debugging

--- commons/smf_sync_with_phrase_app/smf_sync_with_phrase_app.rb
+++ commons/smf_sync_with_phrase_app/smf_sync_with_phrase_app.rb
@@ -44,7 +44,7 @@ lane :smf_sync_with_phrase_app do |options|
     puts "The folowing files changed: #{files_which_changed}"
     if files_which_changed.include? '.strings'
 
-        sh("cd #{project_root_dir} && git add *.strings && git commit *.strings -m \"Updating i18n\"")
+        sh("cd #{project_root_dir} && git add *.strings && git commit -m \"Updating i18n\" *.strings")
         Dir.chdir(project_root_dir) do
             smf_push_to_git_remote(
               tags: false
