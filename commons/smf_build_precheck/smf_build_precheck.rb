d4655227db924a16c0893b0b3f32f6bc92ca8c8a
kondeichmann <k.deichmann@mac.com>
2019-10-23 15:12:42 +0200

    add more messages to precheck

--- commons/smf_build_precheck/smf_build_precheck.rb
+++ commons/smf_build_precheck/smf_build_precheck.rb
@@ -7,7 +7,9 @@ end
 
 def perform_build_precheck_ios(build_variant, build_variant_config)
 
-	if build_variant_config[:upload_itc] == true && !build_variant_config[:itc_apple_id].nil?
+	if build_variant_config[:upload_itc] == true && build_variant_config[:itc_apple_id].nil?
 		UI.error("itc_apple_id not set in Config.json. Please Read: https://smartmobilefactory.atlassian.net/wiki/spaces/SMFIOS/pages/669646876/Missing+itc+apple+id ")
+	else
+		UI.message("Build Precheck: Nothing reportable found")
 	end
 end
