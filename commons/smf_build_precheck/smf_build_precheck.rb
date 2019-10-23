1b26de1623d24839a3f1d99c2069269cadddfe00
kondeichmann <k.deichmann@mac.com>
2019-10-23 15:22:19 +0200

    try adding custom Error

--- commons/smf_build_precheck/smf_build_precheck.rb
+++ commons/smf_build_precheck/smf_build_precheck.rb
@@ -8,8 +8,11 @@ end
 def perform_build_precheck_ios(build_variant, build_variant_config)
 
 	if build_variant_config[:upload_itc] == true && build_variant_config[:itc_apple_id].nil?
-		raise "itc_apple_id not set in Config.json. Please Read: https://smartmobilefactory.atlassian.net/wiki/spaces/SMFIOS/pages/669646876/Missing+itc+apple+id "
+		raise ITCAppleIdError, "itc_apple_id not set in Config.json. Please Read: https://smartmobilefactory.atlassian.net/wiki/spaces/SMFIOS/pages/669646876/Missing+itc+apple+id "
 	else
 		UI.message("Build Precheck: Nothing reportable found")
 	end
+end
+
+class ITCAppleIdError < StandardError
 end
