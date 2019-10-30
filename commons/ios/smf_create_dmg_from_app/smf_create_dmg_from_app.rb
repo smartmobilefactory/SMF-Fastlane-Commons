8b0e48bd3142b8144c7d84433c9594617af69623
UrsKahmann <urs@smartmobilefactory.com>
2019-10-30 18:08:12 +0100

    debugging

--- commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
+++ commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
@@ -7,9 +7,8 @@ private_lane :smf_create_dmg_from_app do |options|
   app_path = smf_path_to_ipa_or_app(build_variant)
 
   # Create the dmg with the script and store it in the same directory as the app
-  log = sh "##{@fastlane_commons_dir_path}/commons/ios/smf_create_dmg_from_app/create_dmg.sh -p #{app_path} -ci #{team_id}"
+  sh "#{@fastlane_commons_dir_path}/commons/ios/smf_create_dmg_from_app/create_dmg.sh -p #{app_path} -ci #{team_id}"
 
-  UI.message("Creatd DMG script log: #{log}")
 
   UI.message("Created dmg at: #{app_path.gsub('.app', '.dmg')}")
 
