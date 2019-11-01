ba7c890c416b884768349c1517dd6807407a09e9
UrsKahmann <urs@smartmobilefactory.com>
2019-11-01 10:59:23 +0100

    debugging

--- commons/ios/smf_get_files_to_attach_to_release/smf_get_files_to_attach_to_release.rb
+++ commons/ios/smf_get_files_to_attach_to_release/smf_get_files_to_attach_to_release.rb
@@ -10,29 +10,29 @@ private_lane :smf_get_files_to_attach_to_release do |options|
 
   # Attach app and test files to release if this is a mac release and the project is in the list for which this should be executed
   if projects.keys.include?(project_name)
-   path_to_files_to_attach = []
-   path_to_ipa_or_app = smf_path_to_ipa_or_app(build_variant)
+    path_to_files_to_attach = []
+    path_to_ipa_or_app = smf_path_to_ipa_or_app(build_variant)
 
-   # check if the path is actually pointing to the .app file
-   if File.extname(path_to_ipa_or_app) != ".app"
-     if File.extname(path_to_ipa_or_app) == ".zip" && File.extname(path_to_ipa_or_app.gsub(".zip", "")) == ".app"
-       sh "unzip -o #{path_to_ipa_or_app}"
-       path_to_ipa_or_app = path_to_ipa_or_app.gsub(".zip", "")
-     else
-       UI.message("Couldn't find the .app file to attach it to the github release 😬")
-       next
+    # check if the path is actually pointing to the .app file
+    if File.extname(path_to_ipa_or_app) != ".app"
+       if File.extname(path_to_ipa_or_app) == ".zip" && File.extname(path_to_ipa_or_app.gsub(".zip", "")) == ".app"
+         sh "unzip -o #{path_to_ipa_or_app}"
+         path_to_ipa_or_app = path_to_ipa_or_app.gsub(".zip", "")
+       else
+         UI.message("Couldn't find the .app file to attach it to the github release 😬")
+         next
+       end
      end
-   end
 
-   if projects[project_name] != nil
-     path_to_renamed_app_file = File.join(File.dirname(path_to_ipa_or_app), "#{projects[project_name]}.app")
-     sh "cp -r #{path_to_ipa_or_app} #{path_to_renamed_app_file}"
-     path_to_files_to_attach.append(path_to_ipa_or_app)
-   end
+     if projects[project_name] != nil
+       path_to_renamed_app_file = File.join(File.dirname(path_to_ipa_or_app), "#{projects[project_name]}.app")
+       sh "cp -r #{path_to_ipa_or_app} #{path_to_renamed_app_file}"
+       path_to_files_to_attach.push(path_to_ipa_or_app)
+     end
 
-   test_dir = "Tests/SMFTests"
-   test_dir_zipped = "#{test_dir}.zip"
-   sh "zip -r \"#{test_dir_zipped}\" \"#{test_dir}\""
-   path_to_files_to_attach.append(test_dir_zipped) # this will be returned
+     test_dir = "Tests/SMFTests"
+     test_dir_zipped = "#{test_dir}.zip"
+     sh "zip -r \"#{test_dir_zipped}\" \"#{test_dir}\""
+     path_to_files_to_attach.push(test_dir_zipped) # this will be returned
   end
 end
