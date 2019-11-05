40979e6ec65cbe84e458060632e47afb228d36ba
kondeichmann <k.deichmann@mac.com>
2019-11-05 14:02:18 +0100

    upload Dsym to sentry

--- setup/flutter_setup.rb
+++ setup/flutter_setup.rb
@@ -148,6 +148,7 @@ private_lane :smf_super_upload_dsyms do |options|
       build_variant: build_variant,
       org_slug: @smf_fastlane_config[:sentry_org_slug],
       project_slug: @smf_fastlane_config[:sentry_project_slug],
+      escaped_filename: build_variant_config[:scheme].gsub(' ', "\ "),
       build_variant_org_slug: build_variant_ios_config[:sentry_org_slug],
       build_variant_project_slug: build_variant_ios_config[:sentry_project_slug]
   )
