813f310cd9f47e769f83964511a13a5f80cbfd92
UrsKahmann <urs@smartmobilefactory.com>
2019-11-07 14:00:21 +0100

    removed swiftlint.yml Now looking for the yml file in the projects root directory

--- commons/smf_run_linter/smf_run_swift_lint.rb
+++ commons/smf_run_linter/smf_run_swift_lint.rb
@@ -4,15 +4,21 @@ SWIFT_LINT_OUTPUT_PATH = 'build/swiftlint.result.json'
 private_lane :smf_run_swift_lint do
 
   swift_lint_executable_path = "#{smf_workspace_dir}/Submodules/SMF-iOS-CommonProjectSetupFiles/SwiftLint/portable_swiftlint/swiftlint"
+  swift_lint_yml = "#{smf_workspace_dir}swiftlint.yml"
 
   if !File.exist?(swift_lint_executable_path)
     UI.important("SwiftLint executable not present at #{swift_lint_executable_path}. Skipping SwiftLint.")
     next
   end
 
+  if !File.exist?(swift_lint_yml)
+    UI.important("SwiftLint .yml not present at #{swift_lint_yml}. Skipping SwiftLint.")
+    next
+  end
+
   swiftlint(
       output_file: smf_swift_lint_output_path,
-      config_file: "#{@fastlane_commons_dir_path}/commons/smf_run_linter/swiftlint.yml",
+      config_file: swift_lint_yml,
       ignore_exit_status: true,
       reporter: "checkstyle",
       executable: swift_lint_executable_path
