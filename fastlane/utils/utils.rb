ce47a8517c5ed2327f9557f4ba5f74eaf17203fb
UrsKahmann <urs@smartmobilefactory.com>
2019-11-22 14:59:20 +0100

    Changed shallow clone depth to 50 to try to fix a danger problem

--- fastlane/utils/utils.rb
+++ fastlane/utils/utils.rb
@@ -165,7 +165,7 @@ end
 def smf_git_pull(branch)
   branch_name = "#{branch}"
   branch_name.sub!('origin/', '')
-  sh "git pull origin #{branch_name} --depth=1 --quiet --allow-unrelated-histories -X theirs"
+  sh "git pull origin #{branch_name} --depth=50 --quiet --allow-unrelated-histories -X theirs"
 end
 
 def smf_update_config(config, message = nil)
