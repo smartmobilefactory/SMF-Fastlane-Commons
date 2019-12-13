b9a0dbe80f7fb1a677ae0cb85be8718b3a13d08b
UrsKahmann <urs@smartmobilefactory.com>
2019-12-13 15:26:51 +0100

    removed indentation

--- commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
+++ commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
@@ -3,7 +3,7 @@ private_lane :smf_create_dmg_from_app do |options|
 
   build_variant = options[:build_variant]
   team_id = options[:team_id]
-  
+
   app_path = smf_path_to_ipa_or_app(build_variant)
 
   # Create the dmg with the script and store it in the same directory as the app
