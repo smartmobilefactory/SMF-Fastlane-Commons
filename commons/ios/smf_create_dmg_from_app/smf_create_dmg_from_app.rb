606cfa757de53254900bf167cc5480c2b1e9e014
Tim Depping <tim-lorenz@depping.org>
2020-01-15 16:38:52 +0100

    debugging

--- commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
+++ commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
@@ -13,7 +13,7 @@ private_lane :smf_create_dmg_from_app do |options|
   app_copy_path = app_path.sub('.app', '_copy.app')
   UI.message("app_copy_path: #{app_copy_path}")
 
-  sh "cd \"#{File.dirname(app_path)}\"; cp \"#{app_path}\" \"#{app_copy_path}\""
+  sh "cd \"#{File.dirname(app_path)}\"; cp \"#{File.basename(app_path)}\" \"#{File.basename(app_copy_path)}\""
 
   # Create the dmg with the script and store it in the same directory as the app
   sh "#{@fastlane_commons_dir_path}/commons/ios/smf_create_dmg_from_app/create_dmg.sh -p #{app_path} -ci '#{signing_id}'"
