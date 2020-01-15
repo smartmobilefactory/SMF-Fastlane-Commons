4572deb1b1cf30c01d535937385476d63d184d04
Tim Depping <tim-lorenz@depping.org>
2020-01-15 17:05:50 +0100

    clean up code

--- commons/smf_upload_to_appcenter/smf_ios_upload_to_appcenter.rb
+++ commons/smf_upload_to_appcenter/smf_ios_upload_to_appcenter.rb
@@ -29,14 +29,12 @@ private_lane :smf_ios_upload_to_appcenter do |options|
 
     raise("Binary file #{app_path} does not exist. Nothing to upload.") unless File.exist?(app_path)
 
-    UI.message("app_path = #{app_path}")
-
     if upload_sparkle
       package_path = "#{app_path}.zip".sub('.dmg', '.app')
       sh "cd \"#{File.dirname(app_path)}\"; zip -r -q \"#{package_path}\" \"./#{escaped_filename}.app\" \"./#{escaped_filename}.dmg\" \"./#{escaped_filename}.html\" \"./#{sparkle_xml_name}\""
       app_path = package_path
-      UI.message('Upload mac app to AppCenter.')
-      UI.message("app_path = #{app_path}")
+
+      UI.message('Upload zipped mac app to AppCenter.')
       appcenter_upload(
           api_token: ENV[$SMF_APPCENTER_API_TOKEN_ENV_KEY],
           owner_name: owner_name,
