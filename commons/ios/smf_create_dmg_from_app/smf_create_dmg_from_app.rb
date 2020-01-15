2882f60ce758e20cd21f22702c042c20204113fa
Tim Depping <tim-lorenz@depping.org>
2020-01-15 11:38:19 +0100

    add copy of app to zip

--- commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
+++ commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
@@ -9,6 +9,9 @@ private_lane :smf_create_dmg_from_app do |options|
 
   app_path = smf_path_to_ipa_or_app(build_variant)
 
+  # Copy the app for later usage
+  FileUtils.cp(app_path, app_path.gsub('.app', '_copy.app'))
+
   # Create the dmg with the script and store it in the same directory as the app
   sh "#{@fastlane_commons_dir_path}/commons/ios/smf_create_dmg_from_app/create_dmg.sh -p #{app_path} -ci '#{signing_id}'"
 
