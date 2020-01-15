d99eebd72e99cfe2c89887b5425c3ceef0dc09f9
Tim Depping <tim-lorenz@depping.org>
2020-01-15 17:07:23 +0100

    add line

--- commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
+++ commons/ios/smf_create_dmg_from_app/smf_create_dmg_from_app.rb
@@ -12,6 +12,7 @@ private_lane :smf_create_dmg_from_app do |options|
   # Create the dmg with the script and store it in the same directory as the app
   sh "#{@fastlane_commons_dir_path}/commons/ios/smf_create_dmg_from_app/create_dmg.sh -p #{app_path} -ci '#{signing_id}'"
 
+
   UI.message("Created dmg at: #{app_path.gsub('.app', '.dmg')}")
 
   app_path.gsub('.app', '.dmg')
