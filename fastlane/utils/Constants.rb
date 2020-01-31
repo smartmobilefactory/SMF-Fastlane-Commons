d88d858394ee30e7954e415e095b4e4fffe08850
Kevin Delord <delord.kevin@gmail.com>
2020-01-31 11:23:34 +0100

    Remove pattern from Jenkinsfile generation

--- fastlane/utils/Constants.rb
+++ fastlane/utils/Constants.rb
@@ -47,5 +47,3 @@ $CHANGELOG_TEMP_FILE = 'temp_changelog.txt'
 $CHANGELOG_TEMP_FILE_HTML = 'temp_changelog.html'
 
 $POD_DEFAULT_VARIANTS = ['patch', 'minor', 'major', 'current', 'breaking', 'internal']
-
-$IOS_BUILD_NODES = ['iosbuild', 'mobileci7', 'mobileci8']
