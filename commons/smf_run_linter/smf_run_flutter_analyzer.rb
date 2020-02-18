130fb9d6b3fa73e7dd0b6b6e7e92d824eb21c995
Tim Depping <tim-lorenz@depping.org>
2020-02-18 09:30:06 +0100

    put flutter analyzer to xml conversion in own method

--- commons/smf_run_linter/smf_run_flutter_analyzer.rb
+++ commons/smf_run_linter/smf_run_flutter_analyzer.rb
@@ -1,8 +1,15 @@
 private_lane :smf_run_flutter_analyzer do |options|
   FLUTTER_ANALYZER_OUTPUT_PATH = "#{smf_workspace_dir}/flutter_analyzer.xml"
   flutter_analyzer_output = sh("cd #{smf_workspace_dir} && #{smf_get_flutter_binary_path} analyze || true")
-  lines = flutter_analyzer_output.split(/\n/)
   flutter_analyzer_file = File.new(FLUTTER_ANALYZER_OUTPUT_PATH, 'w+')
+
+  flutter_analyzer_xml = _smf_flutter_analyzer_output_to_xml(flutter_analyzer_output)
+
+  File.write(flutter_analyzer_file, flutter_analyzer_xml)
+end
+
+def _smf_flutter_analyzer_output_to_xml(output)
+  lines = output.split(/\n/)
 
   builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
     xml.checkstyle {
@@ -15,6 +22,5 @@ private_lane :smf_run_flutter_analyzer do |options|
       end
     }
   end
-
-  File.write(flutter_analyzer_file, builder.to_xml)
+  builder.to_xml
 end
