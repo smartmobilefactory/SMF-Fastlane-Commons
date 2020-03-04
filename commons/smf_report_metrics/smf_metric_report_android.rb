c170b7d338bf897e9159af74f9324289c402b730
timfreiheit <timfreiheit@hotmail.de>
2020-03-04 14:59:58 +0100

    run gradle task

--- commons/smf_report_metrics/smf_metric_report_android.rb
+++ commons/smf_report_metrics/smf_metric_report_android.rb
@@ -100,24 +100,25 @@ end
 # ]
 #
 def smf_owasp_report_android
-    report = []
-    owasp_report = JSON.parse(File.read(smf_get_file_path('dependency-check-report.json')), :symbolize_names => false)
-    owasp_report['dependencies'].each { |dependency|
-        vulnerabilities = dependency['vulnerabilities']
-        if !vulnerabilities.nil? && !vulnerabilities.empty?
-            vulnerabilityNames = vulnerabilities.map { |it| it['name'] }
-            dependency['packages'].each { |package|
-                # example: pkg:maven/com.squareup.okhttp3/okhttp@3.10.0",
-                packageIdMatch = package['id'].match(/pkg:maven\/(.*)@(.*)/)
-                if packageIdMatch
-                    report.append({
-                        'id' => packageIdMatch[1].gsub('/', ':'),
-                        'version' => packageIdMatch[2],
-                        'vulnerabilities' => vulnerabilityNames
-                    })
-                end
-            }
-        end
-    }
-    report
+  gradle(task: 'dependencyCheckAnalyze')
+  report = []
+  owasp_report = JSON.parse(File.read(smf_get_file_path('dependency-check-report.json')), :symbolize_names => false)
+  owasp_report['dependencies'].each { |dependency|
+      vulnerabilities = dependency['vulnerabilities']
+      if !vulnerabilities.nil? && !vulnerabilities.empty?
+          vulnerabilityNames = vulnerabilities.map { |it| it['name'] }
+          dependency['packages'].each { |package|
+              # example: pkg:maven/com.squareup.okhttp3/okhttp@3.10.0",
+              packageIdMatch = package['id'].match(/pkg:maven\/(.*)@(.*)/)
+              if packageIdMatch
+                  report.append({
+                      'id' => packageIdMatch[1].gsub('/', ':'),
+                      'version' => packageIdMatch[2],
+                      'vulnerabilities' => vulnerabilityNames
+                  })
+              end
+          }
+      end
+  }
+  report
 end
