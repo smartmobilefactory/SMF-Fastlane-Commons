8ecb392e53fa3ba4c1369ae54462dba70e2175d4
UrsKahmann <urs@smartmobilefactory.com>
2020-03-23 17:22:06 +0100

    Updated the smf_update_generated_files lane to also be able to update custom jenkins files (for the SPC - Sparkle Package Creator)

--- commons/smf_update_generated_files/README.md
+++ commons/smf_update_generated_files/README.md
@@ -1,8 +1,21 @@
 ## Update Generated Files
-This lane generates the Jenkinsfile an pushes new changes if it was not up to date.
+This lane generates the Jenkinsfile and additional files (optional) and pushes new changes if it was not up to date.
+If you want to update custom jenkins files, you can pass a list of dictionaries to this lane.
+Each dict should contain:
+    - The template path
+    - The path to the generated file
+    - boolean whether or not to include multi-build variant in the jenkins file build variants array.
 
 Example:
 
 ```
-smf_update_generated_setup_files
+smf_update_generated_setup_files(
+    files_to_update: [ {                               \
+            :template => <path to template>,            |
+            :file => <path to file>,                    |
+            :remove_multibuilds => <true or false>      |--- optional parameter
+        },                                              |
+        ...                                             |
+    ]                                                   /
+)
 ```
--- commons/smf_update_generated_files/smf_update_generated_files.rb
+++ commons/smf_update_generated_files/smf_update_generated_files.rb
@@ -1,30 +1,62 @@
-desc "Generates a Jenkinsfile and commits it if there are changes"
-private_lane :smf_update_generated_files do
+desc "Generates a Jenkinsfile (and optional additional files) and commits them if there are changes"
+private_lane :smf_update_generated_files do |options|
 
-    smf_generate_jenkins_file
+  additional_files_to_update = options[:files_to_update].nil? ? [] : options[:files_to_update]
 
-    jenkinsfile_changed = false
-    Dir.chdir(smf_workspace_dir) do
-      jenkinsfile_changed = `git status --porcelain`.include? 'Jenkinsfile'
+  _smf_update_jenkins_file
+
+  additional_files_to_update.each { |file_data|
+    smf_generate_jenkins_file(
+      custom_jenkinsfile_template: file_data[:template],
+      custom_jenkinsfile_path: file_data[:file],
+      remove_multibuild_variants: file_data[:remove_multibuilds]
+    )
+
+    if (_smf_generated_file_changed?(file_data[:file]))
+      _smf_commit_generated_file(file_data[:file], false)
     end
+  }
 
-    UI.message('Checking for Jenkinsfile changes...')
+end
 
-    # If something changed in config
-    if jenkinsfile_changed
-      UI.message('Jenkinsfile changed since last build, will synchronize and commit the changes...')
+def _smf_update_jenkins_file
+  UI.message('Checking for Jenkinsfile changes...')
+  # JENKINSFILE is always updated
+  jenkinsfile_path = File.join(smf_workspace_dir, 'Jenkinsfile')
 
-      git_add(path: './Jenkinsfile')
-      git_commit(path: '.', message: 'Updated Generated Jenkinsfile')
+  smf_generate_jenkins_file
 
-      unlock_keychain(path: 'login.keychain', password: ENV['LOGIN'])
-      unlock_keychain(path: 'jenkins.keychain', password: ENV['JENKINS'])
+  if (_smf_generated_file_changed?(jenkinsfile_path))
+    _smf_commit_generated_file(jenkinsfile_path)
+  else
+    UI.success('Generated Jenkinsfile is up to date. Nothing to do.')
+  end
+end
 
-      smf_push_to_git_remote(remote_branch: ENV['CHANGE_BRANCH'])
+def _smf_generated_file_changed?(path)
+  path = path.sub(smf_workspace_dir, '')
+  file_changed = false
+  Dir.chdir(smf_workspace_dir) do
+    file_changed = `git status --porcelain`.include?(path)
+  end
 
-      UI.error('Generated Jenkinsfile changed since last build, build will be restarted. This is not a failure.')
-      raise "Jenkinsfile changed, restarting buildjob 🔄"
-    else
-      UI.success('Generated Jenkinsfile is up to date. Nothing to do.')
-    end
+  file_changed
+end
+
+def _smf_commit_generated_file(path, fail_build = true)
+  file_name = File.basename(path)
+  UI.message("#{file_name} changed since last build, will synchronize and commit the changes...")
+
+  git_add(path: path)
+  git_commit(path: path, message: "Updated Generated File: #{file_name}")
+
+  unlock_keychain(path: 'login.keychain', password: ENV['LOGIN'])
+  unlock_keychain(path: 'jenkins.keychain', password: ENV['JENKINS'])
+
+  smf_push_to_git_remote(remote_branch: ENV['CHANGE_BRANCH'])
+
+  if (fail_build)
+    UI.error("Generated File: #{file_name} changed since last build, build will be restarted. This is not a failure.")
+    raise "#{file_name} changed, restarting buildjob 🔄"
+  end
 end
