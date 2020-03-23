6d29a0d1813138be01e01463d5d14d7b2fd6e87d
UrsKahmann <urs@smartmobilefactory.com>
2020-03-23 17:24:09 +0100

    added basic functionallity to create sparkel package, WIP

--- tools/smf_sparkle_package_creator.rb
+++ tools/smf_sparkle_package_creator.rb
@@ -1,6 +1,30 @@
 ############## SPARKLE PACKAGE CREATOR ##########
 # This lane is only used for the sparkle package creator
-#
+
 lane :smf_create_sparkle_package do |options|
-  UI.message("Running sparkle package creator")
+  UI.message("Starting sparkle package creator...")
+
+  build_variant = !options[:build_variant].nil? ? options[:build_variant] : smf_get_first_variant_from_config
+  build_variant_config = @smf_fastlane_config[:build_variants][build_variant.to_sym]
+  sparkle_config = build_variant_config[:sparkle]
+
+  smf_upload_with_sparkle(
+    build_variant: build_variant,
+    scheme: build_variant_config[:scheme],
+    sparkle_dmg_path: sparkle_config[:dmg_path],
+    sparkle_version: sparkle_config[:sparkle_version],
+    sparkle_signing_team: sparkle_config[:sparkle_signing_team],
+    sparkle_private_key: sparkle_config[:signing_key]
+  )
+end
+
+
+
+# "CONSTANTS"
+def _smf_spc_template_path
+  File.join(smf_workspace_dir,"#{@fastlane_commons_dir_path}/commons/smf_generate_jenkins_file/Jenkinsfile_Sparkle_Package_Creator.template)")
+end
+
+def _smf_spc_jenkinsfile_path
+  File.join(smf_workspace_dir,'Sparkle-Package-Creator/Sparkle-Package-Creator-Jenkinsfile')
 end
