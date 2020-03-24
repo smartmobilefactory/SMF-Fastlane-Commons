bff3a9a987d4f0b220fd378f52b47d70bcc4741c
UrsKahmann <urs@smartmobilefactory.com>
2020-03-24 10:05:36 +0100

    Added new optional parameters to use a given dmg for package creation

--- commons/ios/smf_upload_with_sparkle/smf_upload_with_sparkle.rb
+++ commons/ios/smf_upload_with_sparkle/smf_upload_with_sparkle.rb
@@ -15,13 +15,18 @@ private_lane :smf_upload_with_sparkle do |options|
   sparkle_xml_name = options[:sparkle_xml_name]
   sparkle_private_key = options[:sparkle_private_key]
 
+  # Optional
+  source_dmg_path = options[:source_dmg_path]
+  custom_app_name = options[:custom_app_name]
+  target_directory = options[:target_directory]
+
   if sparkle_private_key.nil? || ENV[sparkle_private_key].nil?
     UI.error('Sparkle private key is either not set in the Config.json, or there is no credential stored in Jenkins')
     raise 'Error none existing private key credential'
   end
 
-  dmg_path = smf_path_to_dmg(build_variant)
-  update_dir = "#{smf_workspace_dir}/build/"
+  dmg_path = source_dmg_path.nil? ? smf_path_to_dmg(build_variant) : source_dmg_path
+  update_dir = target_directory.nil? ? "#{smf_workspace_dir}/build/" : target_directory
 
   release_notes = smf_read_changelog(html: true)
   release_notes_name = "#{scheme}.html"
@@ -31,7 +36,7 @@ private_lane :smf_upload_with_sparkle do |options|
     raise("DMG file #{dmg_path} does not exit. Nothing to upload.")
   end
 
-  app_name = "#{sparkle_dmg_path}#{scheme}.dmg"
+  app_name = custom_app_name.nil? ? "#{sparkle_dmg_path}#{scheme}.dmg" : custom_app_name
 
   unless sparkle_upload_url.nil? || sparkle_upload_user.nil?
     sh("scp -i #{ENV['CUSTOM_SPARKLE_PRIVATE_SSH_KEY']} #{update_dir}#{release_notes_name} '#{sparkle_upload_user}'@#{sparkle_upload_url}:/#{sparkle_dmg_path}#{release_notes_name}")
--- tools/smf_sparkle_package_creator/smf_sparkle_package_creator.rb
+++ tools/smf_sparkle_package_creator/smf_sparkle_package_creator.rb
@@ -1,9 +1,7 @@
-############## SPARKLE PACKAGE CREATOR ##########
-# This lane is only used for the sparkle package creator
 
 lane :smf_create_sparkle_package do |options|
   UI.message("Starting sparkle package creator...")
-
+=begin
   build_variant = !options[:build_variant].nil? ? options[:build_variant] : smf_get_first_variant_from_config
   build_variant_config = @smf_fastlane_config[:build_variants][build_variant.to_sym]
   sparkle_config = build_variant_config[:sparkle]
@@ -16,6 +14,7 @@ lane :smf_create_sparkle_package do |options|
     sparkle_signing_team: sparkle_config[:sparkle_signing_team],
     sparkle_private_key: sparkle_config[:signing_key]
   )
+=end
 end
 
 
