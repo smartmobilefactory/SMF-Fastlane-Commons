80f818cb2a257bd3468dfab6e157cec887121cab
UrsKahmann <urs@smartmobilefactory.com>
2020-03-25 12:19:32 +0100

    debugging

--- tools/smf_sparkle_package_creator/smf_sparkle_package_creator.rb
+++ tools/smf_sparkle_package_creator/smf_sparkle_package_creator.rb
@@ -4,7 +4,7 @@ REPO = "SMF-Sparkle-Packages-Container"
 lane :smf_create_sparkle_package do |options|
   UI.message("Starting sparkle package creator...")
 
-  UI.message("Input file is at: #{options[:dmg_path]}")
+
   build_variant = !options[:build_variant].nil? ? options[:build_variant] : smf_get_first_variant_from_config
   build_variant_config = @smf_fastlane_config[:build_variants][build_variant.to_sym]
   sparkle_config = build_variant_config[:sparkle]
@@ -31,6 +31,7 @@ lane :smf_create_sparkle_package do |options|
     sparkle_version: sparkle_config[:sparkle_version],
     sparkle_signing_team: sparkle_config[:sparkle_signing_team],
     sparkle_private_key: sparkle_config[:signing_key],
+    sparkle_xml_name: sparkle_config[:xml_name],
     source_dmg_path: input_dmg_path,
     target_directory: target_directory
   )
