968eac518f50f314ef5c2cda19d4dccdb8f78648
UrsKahmann <urs@smartmobilefactory.com>
2020-03-25 08:23:22 +0100

    debugging

--- tools/smf_sparkle_package_creator/smf_sparkle_package_creator.rb
+++ tools/smf_sparkle_package_creator/smf_sparkle_package_creator.rb
@@ -9,8 +9,9 @@ lane :smf_create_sparkle_package do |options|
   input_dmg_path = File.join(smf_workspace_dir, options[:dmg_path])
   target_directory = File.join(smf_workspace_dir, "sparkle_package/")
 
-  Dir.mkdir(target_directory)
+  Dir.mkdir(target_directory, 0777)
   `cp #{input_dmg_path} #{File.join(target_directory, File.basename(input_dmg_path))}`
+  input_dmg_path = File.join(target_directory, File.basename(input_dmg_path))
 
   smf_upload_with_sparkle(
     build_variant: build_variant,
