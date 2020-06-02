59ee171cbcf8a26c70cf06bb7ea6dd52449661d6
Thomas Malval <thomas.malval@smartmobilefactory.com>
2020-06-02 11:14:01 +0200

    Sparkle: escape spaces in scp commands

--- commons/ios/smf_upload_with_sparkle/smf_upload_with_sparkle.rb
+++ commons/ios/smf_upload_with_sparkle/smf_upload_with_sparkle.rb
@@ -71,12 +71,12 @@ private_lane :smf_upload_with_sparkle do |options|
       sh("cp #{dmg_path.shellescape} #{intermediate_directory_path}")
       sh("cp #{appcast_xml.shellescape} #{intermediate_directory_path}")
       sh("cp #{update_dir.shellescape}#{release_notes_name} #{intermediate_directory_path}")
-      sh("scp -i #{ENV['CUSTOM_SPARKLE_PRIVATE_SSH_KEY']} -r #{intermediate_directory_path} '#{sparkle_upload_user}'@#{sparkle_upload_url}:/#{sparkle_dmg_path.shellescape}")
+      sh("scp -i #{ENV['CUSTOM_SPARKLE_PRIVATE_SSH_KEY']} -r #{intermediate_directory_path} '#{sparkle_upload_user}'@#{sparkle_upload_url}:/'#{sparkle_dmg_path.shellescape}'")
     else
       # We upload the three elements directly
-    sh("scp -i #{ENV['CUSTOM_SPARKLE_PRIVATE_SSH_KEY']} #{update_dir.shellescape}#{release_notes_name} '#{sparkle_upload_user}'@#{sparkle_upload_url}:/#{sparkle_dmg_path.shellescape}#{release_notes_name}")
-    sh("scp -i #{ENV['CUSTOM_SPARKLE_PRIVATE_SSH_KEY']} #{dmg_path.shellescape} '#{sparkle_upload_user}'@#{sparkle_upload_url}:/#{sparkle_dmg_path.shellescape}#{app_name.shellescape}.dmg")
-    sh("scp -i #{ENV['CUSTOM_SPARKLE_PRIVATE_SSH_KEY']} #{appcast_xml.shellescape} '#{sparkle_upload_user}'@#{sparkle_upload_url}:/#{sparkle_dmg_path.shellescape}#{appcast_upload_name}")
+    sh("scp -i #{ENV['CUSTOM_SPARKLE_PRIVATE_SSH_KEY']} #{update_dir.shellescape}#{release_notes_name} '#{sparkle_upload_user}'@#{sparkle_upload_url}:'/#{sparkle_dmg_path.shellescape}#{release_notes_name}'")
+    sh("scp -i #{ENV['CUSTOM_SPARKLE_PRIVATE_SSH_KEY']} #{dmg_path.shellescape} '#{sparkle_upload_user}'@#{sparkle_upload_url}:'/#{sparkle_dmg_path.shellescape}#{app_name.shellescape}.dmg'")
+    sh("scp -i #{ENV['CUSTOM_SPARKLE_PRIVATE_SSH_KEY']} #{appcast_xml.shellescape} '#{sparkle_upload_user}'@#{sparkle_upload_url}:'/#{sparkle_dmg_path.shellescape}#{appcast_upload_name}'")
     end
   end
 end
