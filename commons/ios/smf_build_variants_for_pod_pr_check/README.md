8d9e986376e28b7d94e04638f605d9efa88cc6f9
Kevin Delord <delord.kevin@gmail.com>
2020-09-04 10:31:43 +0200

    lane smf_build_variants_for_pod_pr_check: use build_variant from parameters

--- commons/ios/smf_build_variants_for_pod_pr_check/README.md
+++ commons/ios/smf_build_variants_for_pod_pr_check/README.md
@@ -1,12 +1,20 @@
 ### Build Variants for Pod PR Check
 This lane returns an array which contains all build variants which need to be build for a PR check.
 
-If the is a build variant specified in the Config.json which contains *alpha* and array containing this build variant will be returned. In case there are multiple alpha build variants, only the first one will be returned in an array.
+If a specific build_variant has been specified in the pipeline then only that one is returned as an array.
 
-Otherwise an array containing all build variants which contain *example* will be returned.
+If no parameters are given then this lane will search for a build variants in the Config.json containing either (and in this priority order) *alpha* then *unittests* then *example*.
 
-Example:
+In case there are multiple *alpha* build variants, only the first one will be returned in an array.
+
+Example without parameters to check build_variants from the Config.json:
 
 ```
 smf_build_variants_for_pod_pr_check()
+```
+
+Example with parameters to use a specific build_variant:
+
+```
+smf_build_variants_for_pod_pr_check(options)
 ```
--- commons/ios/smf_build_variants_for_pod_pr_check/smf_build_variants_for_pod_pr_check.rb
+++ commons/ios/smf_build_variants_for_pod_pr_check/smf_build_variants_for_pod_pr_check.rb
@@ -1,29 +1,36 @@
-private_lane :smf_build_variants_for_pod_pr_check do
+private_lane :smf_build_variants_for_pod_pr_check do |options|
 
+  #####
+  #TODO: Check if this works without any given paramters (pipeline: PR check)
+  #####
+
+  matching_build_variants = []
   all_build_variants = @smf_fastlane_config[:build_variants].keys.map(&:to_s)
 
-  # Check for alpha in build variants.
+  # Check for alpha in build variants of hte Config.json and returns the first one found.
   alpha_build_variant = all_build_variants.detect { |variant| variant.match(/.*alpha.*/) }
 
-  # If there is an alpha return this alpha in an array. Otherwise return all build variants which contain 'example'.
+  # If a specific build_variant has been specified in the pipeline use it instead of the other ones.
+  if !options[:build_variant].nil?
+    matching_build_variants = [options[:build_variant]] # make an array out of it
+  # If there is no alpha return all build variants which contain 'example' or 'unittests'.
   if alpha_build_variant.nil?
     matching_build_variants = _smf_build_variants_matching(/.*example.*/, all_build_variants)
 
     if matching_build_variants.empty?
       matching_build_variants = _smf_build_variants_matching(/.*unittests.*/, all_build_variants)
     end
-
-    if matching_build_variants.empty?
-      UI.error("Error, couldn't find any build variants containing 'example' or 'unittests' in their name.")
-      raise "Error finding matching build variants"
-    end
-
+  # If there is an alpha, use it as default build_variant
   else
     matching_build_variants = [alpha_build_variant]
   end
 
-  matching_build_variants = [matching_build_variants.first]
-  UI.important("Found matching build variants: #{matching_build_variants} for PR check.")
+  if matching_build_variants.empty?
+    UI.error("Error, couldn't find any build variants containing 'alpha', 'example' or 'unittests' in their name.")
+    raise "Error finding matching build variants"
+  else
+    UI.important("Found matching build variants: #{matching_build_variants}.")
+  end
 
   matching_build_variants
 end
