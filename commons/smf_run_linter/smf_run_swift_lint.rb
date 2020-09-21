e3e852855678f33cc9a3f6010349d0f733877b5d
Kevin Delord <delord.kevin@gmail.com>
2020-09-21 18:36:09 +0200

    Swiftlint: create output base directory if does not exist

--- commons/smf_run_linter/smf_run_swift_lint.rb
+++ commons/smf_run_linter/smf_run_swift_lint.rb
@@ -1,8 +1,12 @@
+require 'fileutils'
+
 # Constants
+SWIFT_LINT_OUTPUT_BASE_DIR = 'build/'
 SWIFT_LINT_OUTPUT_PATH = 'build/swiftlint.result.json'
 SWIFT_LINT_RULES_REPORT_PATH = 'build/swiftlint-rules-report.txt'
 
 private_lane :smf_run_swift_lint do
+  _create_output_base_folder()
 
   swift_lint_executable_path = "#{smf_workspace_dir}/Submodules/SMF-iOS-CommonProjectSetupFiles/SwiftLint/portable_swiftlint/swiftlint"
   swift_lint_yml = "#{smf_workspace_dir}/.swiftlint.yml"
@@ -30,6 +34,14 @@ private_lane :smf_run_swift_lint do
   swift_lint_report = "#{smf_workspace_dir}/Submodules/SMF-iOS-CommonProjectSetupFiles/SwiftLint/check_missing_rule_configurations.sh"
   if File.exist?(swift_lint_report)
     sh("#{swift_lint_report} #{smf_workspace_dir} #{smf_workspace_dir}/#{SWIFT_LINT_RULES_REPORT_PATH}")
+  end
+end
+
+def _create_output_base_folder
+  dirname = File.dirname("#{smf_workspace_dir}/#{SWIFT_LINT_OUTPUT_BASE_DIR}")
+  # create dir only if not existing
+  unless File.directory?(dirname)
+    FileUtils.mkdir_p(dirname)
   end
 end
 
