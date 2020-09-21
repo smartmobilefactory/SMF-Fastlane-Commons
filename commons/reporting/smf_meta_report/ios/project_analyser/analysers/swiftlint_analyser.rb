157dc5ea8ef2a49091349bd1302a2f2e837a6a05
Kevin Delord <delord.kevin@gmail.com>
2020-09-21 17:19:51 +0200

    Write logic to analyse swiftlint report

--- commons/reporting/smf_meta_report/ios/project_analyser/analysers/swiftlint_analyser.rb
+++ commons/reporting/smf_meta_report/ios/project_analyser/analysers/swiftlint_analyser.rb
@@ -4,37 +4,29 @@ module SwiftlintAnalyser
 
   SWIFT_LINT_JSON_PATH = ".MetaJSON/swiftlint.json"
 
+  @swiftlint_error_count = nil
+
   # returns a tupel describing the status and what the error is
   # first tupel entry is the status: OK, WARNING, ERROR
   # seconde tupel entry is a message
   def self.verification(src_root)
     UI.message("Verifying #{self.to_s}")
-    # if File.exist?(File.join(src_root, SWIFT_LINT_JSON_PATH)) == true
-    #   return :OK
-    # else
-    #   UI.important("Couldn't locate swiftlint.json at #{SWIFT_LINT_JSON_PATH}")
-    #   return :WARNING
-    # end
+
+    if File.file?(smf_swift_lint_output_path) == false
+      UI.important("Couldn't locate swiftlint report at #{smf_swift_lint_output_path}")
+      return :WARNING
+    end
+
+    swiftlint_report = File.read(swiftlint_report_path)
+    swiftlint_json = JSON.parse(swiftlint_report)
+    @swiftlint_error_count = swiftlint_json.count
+
+    return :OK
   end
 
   # returns the analysed property
   def self.analyse(src_root)
     UI.message("Analysing #{self.to_s} ...")
-    # if self.validate(src_root) == :WARNING
-    #   return
-    # end
-
-    # UI.message("Starting analysis")
-    # return { :content => File.join(src_root, SWIFT_LINT_JSON_PATH), :is_raw => false, :file => :swiftlint_json }
+    return @swiftlint_error_count
   end
 end
-
-
-# TODO:
-# - read and parse the swiftlint.report.json file
-# - count the number of warnings and retain them with the key: 'swiftlint_warnings'
-
-# Legacy code:
-# swiftlint_json_content = File.read(swiftlint_report_path)
-# swiftlint_json = JSON.parse(swiftlint_json_content, :symbolize_names => false)
-# swiftlint_error_count = swiftlint_json.count
