6906f4f821e5abf0f9be83aa06711bca85d0ac02
Kevin Delord <delord.kevin@gmail.com>
2020-09-24 15:32:32 +0200

    Refactor to reduce functions and improve logic

--- commons/smf_danger/smf_danger_repo_clean_up.rb
+++ commons/smf_danger/smf_danger_repo_clean_up.rb
@@ -25,19 +25,19 @@ def _smf_deprecated_files_for_platform
     raise "Unknown platform: #{@platform.to_s}"
   end
 
+  deprecated_files = deprecated_files.map { |key| key.to_s }
   deprecated_files
 end
 
 def _smf_check_config_project_missing_required_keys
-  project_config = @smf_fastlane_config[:project]
-  if project_config.nil?
-    UI.error("[ERROR]: Missing 'project' info in Config.json")
-  end
-
-  required_keys = _smf_required_config_keys_for_platform
+  # Convert all keys to string for easier matching
+  project_config_keys = _get_config(:project).keys.map { |key| key.to_s }
+  # Check for missing required keys in the 'config.project' hash
   missing_required_keys = []
-  required_keys.each do |key|
-    unless project_config.keys.include?(required_keys.to_s)
+  _smf_required_config_keys_for_platform.each do |key|
+    # If the required key is NOT present in the config.project hash
+    # Then display a warning about it using Danger to ask the dev the add it to the Config.json.
+    unless project_config_keys.include?(key.to_s)
       missing_required_keys.push(key.to_s)
     end
   end
@@ -46,17 +46,15 @@ def _smf_check_config_project_missing_required_keys
 end
 
 def _smf_check_config_project_allowed_keys_only
-  project_config = @smf_fastlane_config[:project]
-  if project_config.nil?
-    UI.error("[ERROR]: Missing 'project' info in Config.json")
-  end
-
-  required_keys = _smf_required_config_keys_for_platform
+  # Convert all keys to string for easier matching
+  project_config_keys = _get_config(:project).keys.map { |key| key.to_s }
+  # Check for non-allowed (or deprecated) keys in the 'config.project' hash
   deprecated_keys = []
-  project_config.keys.each do |key|
-    # Retain the key if it is NOT required (eg. allowed) to warn the dev about it.
-    unless required_keys.include?(key.to_s)
-      deprecated_keys.push(key.to_s)
+  project_config_keys.each do |key|
+    # If the key is NOT one of the allowed (and required) keys
+    # Then display a warning about it using Danger to ask the dev to remove it from the Config.json.
+    unless _smf_required_config_keys_for_platform.include?(key)
+      deprecated_keys.push(key)
     end
   end
 
@@ -77,23 +75,21 @@ def _smf_required_config_keys_for_platform
     raise "Unknown platform: #{@platform.to_s}"
   end
 
+  required_keys = required_keys.map { |key| key.to_s }
   required_keys
 end
 
 def _smf_check_config_build_variant_keys
-  build_variants = @smf_fastlane_config[:build_variants]
-  if build_variants.nil? || build_variants.count == 0
-    UI.error("[ERROR]: Missing or empty 'build_variants' in Config.json")
-  end
-
-  deprecated_keys = _smf_deprecated_build_variant_keys_for_platform
   deprecated_keys_in_variant = []
-  build_variants.each do |build_variant, build_variant_info|
-    build_variant_info.keys.each do |key|
-      # For each build_variant
-      # Checks for keys that have been marked as deprecated
-      if deprecated_keys.include?(key.to_s)
-        deprecated_keys_in_variant.push("#{build_variant}.#{key.to_s}")
+  # For each build_variant present in the Config.json
+  _get_config(:build_variants).each do |build_variant, build_variant_info|
+    # Convert all build_variant keys to string for easier matching
+    build_variant_info_keys = build_variant_info.keys.map { |key| key.to_s }
+    build_variant_info_keys.each do |key|
+      # Checks for existing keys that have been marked as deprecated
+      if _smf_deprecated_build_variant_keys_for_platform.include?(key)
+        # Use Danger to ask the dev to remove the key from the Config.json
+        deprecated_keys_in_variant.push("#{build_variant}.#{key}")
       end
     end
   end
@@ -115,5 +111,16 @@ def _smf_deprecated_build_variant_keys_for_platform
     raise 'Unknown platform: "#{@platform.to_s}"'
   end
 
+  deprecated_keys = deprecated_keys.map { |key| key.to_s }
   deprecated_keys
+end
+
+def _get_config(key)
+  config = @smf_fastlane_config[key]
+  if config.nil? || config.count == 0
+    raise "[ERROR]: Missing or empty '#{key.to_s}' in Config.json"
+    return []
+  end
+
+  config
 end
