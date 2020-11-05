3e1e3fb81f4f1fbaf433ec85e4a4f5d06b786a05
Kevin Delord <delord.kevin@gmail.com>
2020-11-05 11:23:32 +0100

    fix conflicting chdir block

--- commons/reporting/smf_android_monitor_unit_tests/smf_android_monitor_unit_tests.rb
+++ commons/reporting/smf_android_monitor_unit_tests/smf_android_monitor_unit_tests.rb
@@ -2,26 +2,20 @@ require 'date'
 
 private_lane :smf_android_monitor_unit_tests do |options|
 
-  project_dir = smf_workspace_dir
-  # unless Dir.exist?(project_dir)
-  #   UI.messsage("The directory '#{project_dir}' does not exist")
-  #   raise 'Missing test result directory'
-  # end
+  test_count = 0
 
   # Change directory to search for test result files
-  Dir.chdir(project_dir)
-
-  test_results = Dir.glob('**/build/test-results/**/TEST-*.xml')
-  if test_results.empty?
-    UI.message("No test result files found in #{project_dir}")
-  end
-
-  test_count = 0
-  test_results.each do |test_result|
-    File.open(test_result,"r") do |file|
-      text = file.read
-      if test_result_match = text.match("tests=\"([0-9]+)\"")
-        test_count += test_result_match.captures[0].to_i
+  Dir.chdir(smf_workspace_dir) do
+    test_results = Dir.glob('**/build/test-results/**/TEST-*.xml')
+    if test_results.empty?
+      UI.message("No test result files found in #{smf_workspace_dir}")
+    end
+    test_results.each do |test_result|
+      File.open(test_result,"r") do |file|
+        text = file.read
+        if test_result_match = text.match("tests=\"([0-9]+)\"")
+          test_count += test_result_match.captures[0].to_i
+        end
       end
     end
   end
