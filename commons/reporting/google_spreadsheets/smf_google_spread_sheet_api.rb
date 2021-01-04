e84a528b7c3cfc832b96458f74db54d4aa5c8aab
Kevin Delord <delord.kevin@gmail.com>
2021-01-04 16:06:26 +0100

    Add code for the financial reporting

--- commons/reporting/google_spreadsheets/smf_google_spread_sheet_api.rb
+++ commons/reporting/google_spreadsheets/smf_google_spread_sheet_api.rb
@@ -1,4 +1,7 @@
+#!/usr/bin/ruby
+require 'json'
 require 'net/http'
+require 'date'
 
 # Retrieves a temporary access token to the google spread sheets
 # use to then upload/add/append new data to google spread sheets
@@ -60,6 +63,39 @@ def smf_google_api_append_data_to_spread_sheet(sheet_id, sheet_name, data)
   end
 end
 
+# Using a temporary access token, get the data from an online spreadsheet
+def smf_google_api_get_data_from_spread_sheet(sheet_id, sheet_name)
+  bearer_token = _smf_google_api_get_bearer_token
+
+  # Delay of 5 seconds to let Google's servers propagate the new access_token
+  # https://stackoverflow.com/a/42771170/2790648
+  # Added on 14.09.2020 due to random "Internal Server Error (RuntimeError)"
+  sleep(5)
+
+  sheet_uri = URI.parse"https://sheets.googleapis.com/v4/spreadsheets/#{sheet_id}/values/#{sheet_name}"
+
+  request = Net::HTTP::Get.new(sheet_uri)
+  request.content_type = 'application/json'
+  request['Accept'] = 'application/json'
+  request['Authorization'] = "Bearer #{bearer_token}"
+
+  response = Net::HTTP.start(sheet_uri.hostname, sheet_uri.port, use_ssl: true ) do |client|
+    client.request(request)
+  end
+
+  case response
+  when Net::HTTPSuccess
+    begin
+      body = JSON.parse(response.body)
+      return body.dig('values')
+    rescue
+      raise 'Error parsing response body'
+    end
+  else
+    raise "Error fetching spreadsheet: #{response.message}"
+  end
+end
+
 # Takes sheet_entries (array) and reporting_type to gather the necessary keys
 # for a valid JSON reporting to the online spreadsheet.
 def smf_create_sheet_data_from_entries(sheet_entries, reporting_type)
@@ -71,6 +107,8 @@ def smf_create_sheet_data_from_entries(sheet_entries, reporting_type)
       values.push(_smf_automatic_reporting_spreadsheet_entry_to_line(entry))
     elsif reporting_type == :META_REPORTING
       values.push(_smf_meta_reporting_spreadsheet_entry_to_line(entry))
+    elsif reporting_type == :FINANCIAL_REPORTING
+      values.push(_smf_financial_reporting_spreadsheet_entry_to_line(entry))
     end
   end
 
@@ -116,6 +154,12 @@ def _smf_meta_reporting_spreadsheet_entry_to_line(entry)
   # The order of the elements in this array directly correspond to the table columns in the google spread sheet
   # thus it is VERY IMPORTANT to not change the order!
   [entry[:date], entry[:repo], entry[:platform], entry[:branch], entry[:xcode_version], entry[:idfa], entry[:bitcode], entry[:swiftlint_warnings], entry[:ats], entry[:swift_version]]
+end
+
+def _smf_financial_reporting_spreadsheet_entry_to_line(entry)
+  # The order of the elements in this array directly correspond to the table columns in the google spread sheet
+  # thus it is VERY IMPORTANT to not change the order!
+  [entry[:date], entry[:project], entry[:sales], entry[:expenses], entry[:target], entry[:actual], entry[:daily_rate], entry[:ratio]]
 end
 
 def _smf_unwrap_value(value)
