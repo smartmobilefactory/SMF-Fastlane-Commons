d50988c701c90cdd41b957cb7e510d1f76b1e0f0
Kevin Delord <delord.kevin@gmail.com>
2021-01-19 17:54:24 +0100

    Add lane for macOS reporting

--- setup/macos_setup.rb
+++ setup/macos_setup.rb
@@ -26,6 +26,10 @@ lane :smf_setup_dependencies_pr_check do |options|
   smf_super_setup_dependencies(options)
 end
 
+lane :smf_pod_setup_dependencies_pr_check do |options|
+  smf_super_setup_dependencies(options)
+end
+
 lane :smf_setup_dependencies_build do |options|
   smf_super_setup_dependencies(options)
 end
