7a91aa58b758b2f9252bcc4f32e5db3f495b40e5
UrsKahmann <urs.kahmann@gmx.de>
2021-02-19 09:55:03 +0100

    Adde android. phrase sync wrapper

---
+++ commons/smf_sync_with_phrase/android_wrapper.rb
@@ -0,0 +1,8 @@
+
+# Wrapper lane so android projects keep working after the newly implemented
+# phrase sync lane. Should be removed as soon as all android projects are migrated.
+
+private_lane :sync_with_phrase_app do |options|
+  UI.warn('⚠️⚠️⚠️️The lane/action "sync_with_phrase_app" is deprecated, please use "smf_sync_with_phrase"! ⚠️⚠️⚠️')
+  smf_sync_with_phrase(options)
+end
