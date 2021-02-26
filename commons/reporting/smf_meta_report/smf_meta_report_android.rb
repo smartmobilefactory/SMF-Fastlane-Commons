0a43c0996b83ea8c9815ef66edc33198f1583b06
Kevin Delord <delord.kevin@gmail.com>
2021-02-26 11:36:39 +0100

    create file for smf_meta_report_android

--- commons/reporting/smf_meta_report/smf_meta_report_android.rb
+++ commons/reporting/smf_meta_report/smf_meta_report_android.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/ruby
 require 'date'
 
-def smf_meta_report_ios(options)
+def smf_meta_report_android(options)
   # Analysis
-  analysis_data = _smf_analyse_ios_project(options)
+  analysis_data = _smf_analyse_android_project(options)
 
-  if _should_send_ios_report_data(analysis_data)
+  if _should_send_android_report_data(analysis_data)
     # Format and prepare data for uploading
     upload_data = _smf_create_meta_report_to_upload(analysis_data)
 
@@ -17,36 +17,25 @@ end
 # Returns true only when the :branch value respect the required
 # format. The goal is to avoid reports for useless-testing branches.
 # Accepted format:
-# - 'master': branch strictly named master
-# - '<version>/master': where version is only digit with optional 'decimal'
-#      examples: '12/master', '3.4/master'
-def _should_send_ios_report_data(options)
-  if options[:branch].match(/^master$/) # iOS (A-Team)
-    return true
+# - 'dev' or 'kmpp': branches strictly named
+def _should_send_android_report_data(options)
+  if branch.match(/^dev$/) # Android
+    return false
   end
 
-  if options[:branch].match(/^\d+[\d\.]*\/master$/) # iOS (Strato-Team)
-    return true
+  if branch.match(/^kmpp$/) # Android (Eismann - temporary)
+    return false
   end
 
   return false
 end
 
-def _smf_analyse_ios_project(options)
-  xcode_settings = smf_xcodeproj_settings(options)
-
+def _smf_analyse_android_project(options)
   analysis_json = {}
   analysis_json[:date] = Date.today.to_s
   analysis_json[:repo] = @smf_fastlane_config[:project][:project_name]
   analysis_json[:platform] = smf_meta_report_platform_friendly_name
   analysis_json[:branch] = ENV['BRANCH_NAME']
-  analysis_json[:xcode_version] = @smf_fastlane_config[:project][:xcode_version]
-  analysis_json[:idfa] = smf_analyse_idfa_usage
-  analysis_json[:bitcode] = smf_analyse_bitcode(xcode_settings, options)
-  analysis_json[:swiftlint_warnings] = smf_swift_lint_number_of_warnings
-  analysis_json[:ats] = smf_analyse_ats_exception
-  analysis_json[:swift_version] = smf_analyse_swift_version(xcode_settings, options)
-  analysis_json[:deployment_target] = smf_analyse_deployment_targets(xcode_settings, options)
 
   return analysis_json
 end
