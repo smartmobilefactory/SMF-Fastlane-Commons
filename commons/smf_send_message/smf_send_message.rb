768cb5684f4ddc8f9ff8dca4c2f1b793379cefc2
UrsKahmann <urs.kahmann@gmx.de>
2021-03-22 15:50:21 +0100

    changed back to true

--- commons/smf_send_message/smf_send_message.rb
+++ commons/smf_send_message/smf_send_message.rb
@@ -19,7 +19,7 @@ def _smf_should_skip_notifications_for_branch
     return false
   end
 
-  return false # TODO set to true, changed to false for testing
+  return true
 end
 
 private_lane :smf_send_message do |options|
