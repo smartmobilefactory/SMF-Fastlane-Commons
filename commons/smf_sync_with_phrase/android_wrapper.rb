f2bded05c24b86c8500f85e02dc3cf72d98e7ebe
UrsKahmann <urs.kahmann@gmx.de>
2021-03-22 14:08:04 +0100

    Sending deprecation warning to slack channels for android, too

--- commons/smf_sync_with_phrase/android_wrapper.rb
+++ commons/smf_sync_with_phrase/android_wrapper.rb
@@ -3,6 +3,18 @@
 # phrase sync lane. Should be removed as soon as all android projects are migrated.
 
 private_lane :sync_with_phrase_app do |options|
-  UI.message('⚠️⚠️⚠️️The lane/action "sync_with_phrase_app" is deprecated, please use "smf_sync_with_phrase"! ⚠️⚠️⚠️')
+  message = '⚠️⚠️⚠️️The lane/action "sync_with_phrase_app" is deprecated, please use "smf_sync_with_phrase"! ⚠️⚠️⚠️'
+  UI.message(message)
+
+  name = @smf_fastlane_config.dig(:project, :project_name)
+  slack_channel = @smf_fastlane_config.dig(:project, :slack_channel)
+
+  smf_send_message(
+    title: "⚠️ WARNING: #{name} uses deprecated phraseapp action ⚠️",
+    message: message,
+    type: 'error',
+    slack_channel: slack_channel
+  )
+
   smf_sync_with_phrase(options)
 end
