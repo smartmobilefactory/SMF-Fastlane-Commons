b271b2e692c5545bbe0b12f77c57ef761650d7e9
UrsKahmann <urs.kahmann@gmx.de>
2021-04-13 15:07:03 +0200

    removed debug prints, enabled slack messages for testing

--- commons/smf_send_message/smf_send_message.rb
+++ commons/smf_send_message/smf_send_message.rb
@@ -19,7 +19,7 @@ def _smf_should_skip_notifications_for_branch
     return false
   end
 
-  return true
+  return false # TODO: change to true before merge
 end
 
 private_lane :smf_send_message do |options|
