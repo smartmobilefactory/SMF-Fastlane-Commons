e460574ea10a20442196cb4c92c8b9ae5455f8f4
UrsKahmann <urs.kahmann@gmx.de>
2021-04-13 14:48:29 +0200

    fixed wrong passing of arguments

--- commons/smf_generate_jenkins_file/smf_generate_jenkins_file.rb
+++ commons/smf_generate_jenkins_file/smf_generate_jenkins_file.rb
@@ -130,7 +130,7 @@ def _smf_custom_credential_deprecation_warning
   case @platform
   when :ios, :macos, :apple
     UI.message('Checking for deprecation') # TODO: remove
-    custom_credentials = smf_config_get(nil, [:project, :custom_credentials])
+    custom_credentials = smf_config_get(nil, :project, :custom_credentials)
     UI.message(custom_credentials)
     if custom_credentials.nil? == false && custom_credentials.empty? == false
       _, credential_value = custom_credentials.first
