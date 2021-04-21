5b4b0ebb4000ab6a9f83ad9d3ba8c6cdc0a2d64f
UrsKahmann <urs.kahmann@gmx.de>
2021-04-21 13:35:21 +0200

    Added functionallity to read ticket tags as array

--- commons/smf_git_changelog/smf_read_changelog.rb
+++ commons/smf_git_changelog/smf_read_changelog.rb
@@ -1,18 +1,20 @@
 
 private_lane :smf_read_changelog do |options|
-  changelog = nil
+
   case options[:type]
   when :html
     UI.message("Reading changelog from #{_smf_changelog_html_temp_path}")
-    changelog = File.read(_smf_changelog_html_temp_path).to_s
+    result = File.read(_smf_changelog_html_temp_path).to_s
   when :slack_markdown
     UI.message("Reading changelog from #{_smf_changelog_slack_markdown_temp_path}")
-    changelog = File.read(_smf_changelog_slack_markdown_temp_path).to_s
+    result = File.read(_smf_changelog_slack_markdown_temp_path).to_s
+  when :ticket_tags
+    UI.message("Reading ticket tags from #{_smf_ticket_tags_temp_path}")
+    result = File.read(_smf_ticket_tags_temp_path).to_s.split(' ')
   else
     UI.message("Reading changelog from #{_smf_changelog_temp_path}")
-    changelog =  File.read(_smf_changelog_temp_path).to_s
+    result =  File.read(_smf_changelog_temp_path).to_s
   end
 
-  # return the changelog as string
-  changelog
+  result
 end
