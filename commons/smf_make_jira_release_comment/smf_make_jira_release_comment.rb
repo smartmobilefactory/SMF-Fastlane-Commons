31b65d0a882ae562e1932c4abbcaf9007a10cf1a
UrsKahmann <urs.kahmann@gmx.de>
2021-04-21 13:36:54 +0200

    Implemented lane to comment the release on all involved tickets

--- commons/smf_make_jira_release_comment/smf_make_jira_release_comment.rb
+++ commons/smf_make_jira_release_comment/smf_make_jira_release_comment.rb
@@ -1,5 +1,16 @@
-desc 'Comments the on all jira tickets, released in a new build'
+desc 'Comments the build number and variant on all jira tickets released in a new build'
 
 private_lane :smf_make_jira_realease_comment do |options|
+
+  build_variant = options[:build_variant]
+
+  comment = "Released in #{smf_get_default_name_of_app(build_variant)}"
+
+  ticket_tags = smf_read_changelog(type: :ticket_tags)
+
+  ticket_tags.each do |tag|
+    UI.message("Commenting: #{comment} on ticket with tag: #{tag}")
+    smf_jira_add_comment_to_ticket(tag, comment)
+  end
 
 end
