460003107a79fc0ef642692e404f04b4b0308d4d
Thomas Malval <thomas.malval@smartmobilefactory.com>
2021-06-11 17:02:12 +0200

    Make template path a real path

--- setup/apple_setup.rb
+++ setup/apple_setup.rb
@@ -216,7 +216,9 @@ private_lane :smf_super_create_dmg_and_gatekeeper do |options|
   # The `dmg_template_path` key can be at the project level or at the build_variant level. The build_variant level overrides the project one.
   # If nothing is found, no template will be used for the DMG creation
   dmg_template_path = smf_config_get(build_variant, :dmg_template_path) ?? smf_config_get(nil, :dmg_template_path)
+  dmg_template_path = (smf_workspace_dir + dmg_template_path) unless dmg_template_path.nil?
 
+  # TODO: update doc
   dmg_path = smf_create_dmg_from_app(
     team_id: smf_config_get(build_variant, :team_id),
     code_signing_identity: smf_config_get(build_variant, :code_signing_identity),
