075b75d93b038e9e0b38c2fe949685d4aaca4b06
UrsKahmann <urs.kahmann@gmx.de>
2021-08-06 10:41:18 +0200

    debugging

--- commons/reporting/google_spreadsheets/smf_google_spread_sheet_api.rb
+++ commons/reporting/google_spreadsheets/smf_google_spread_sheet_api.rb
@@ -97,7 +97,9 @@ def smf_google_api_upload_csv_to_spreadsheet(spreadsheet_id, sheet_id, csv_data)
     }]
   }
 
-  request.body = data.to_s #.to_json.gsub('\\', '')
+  request.body = data.to_json
+
+  File.write("./debugging_request.json", data.to_json)
 
   _smf_google_api_start_request(request, uri)
 end
