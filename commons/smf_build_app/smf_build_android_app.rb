8202d0a529d0fb598cc24f6b2c1ed82ad494561a
UrsKahmann <urs.kahmann@gmx.de>
2021-10-07 13:34:21 +0200

    combining task and build variant

--- commons/smf_build_app/smf_build_android_app.rb
+++ commons/smf_build_app/smf_build_android_app.rb
@@ -17,9 +17,12 @@ private_lane :smf_build_android_app do |options|
     end
   end
 
+  letters = build_variant.split('')
+  letters[0] = letters[0].upcase if letters.length >= 1
+  build_variant = letters.join('')
+
   gradle(
-    tasks: ['assemble', 'bundle'],
-    flavor: build_variant,
+    tasks: ["assemble#{build_variant}", "bundle#{build_variant}"],
     properties: properties
   )
 
