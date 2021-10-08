a2c22728e39c1c57a774a4aef740ef2c8a0a150d
UrsKahmann <33418462+UrsKahmann@users.noreply.github.com>
2021-10-08 10:46:29 +0200

    SMFIT-1937: Android-Build-Lane: Generating Android App Bundle when key_store is set (#231)

--- commons/smf_build_app/smf_build_android_app.rb
+++ commons/smf_build_app/smf_build_android_app.rb
@@ -3,22 +3,30 @@ private_lane :smf_build_android_app do |options|
   build_variant = !options[:build_variant].nil? ? options[:build_variant] : ''
   keystore_folder = options[:keystore_folder]
 
-  addition = ''
+  letters = build_variant.split('')
+  letters[0] = letters[0].upcase if letters.length >= 1
+  build_variant = letters.join('')
+
+  tasks = ["assemble#{build_variant}"]
 
   unless keystore_folder.nil?
     keystore_values = smf_pull_keystore(folder: keystore_folder)
 
     if keystore_values[:keystore_file]
-      addition = " -Pandroid.injected.signing.store.file='#{keystore_values[:keystore_file]}'"
-      addition << " -Pandroid.injected.signing.store.password='#{keystore_values[:keystore_password]}'"
-      addition << " -Pandroid.injected.signing.key.alias='#{keystore_values[:keystore_key_alias]}'"
-      addition << " -Pandroid.injected.signing.key.password='#{keystore_values[:keystore_key_password]}'"
+
+      tasks.push("bundle#{build_variant}")
+      properties = {
+        "android.injected.signing.store.file" => keystore_values[:keystore_file],
+        "android.injected.signing.store.password" => keystore_values[:keystore_password],
+        "android.injected.signing.key.alias" => keystore_values[:keystore_key_alias],
+        "android.injected.signing.key.password" => keystore_values[:keystore_key_password]
+      }
+
     end
   end
 
-  letters = build_variant.split('')
-  letters[0] = letters[0].upcase if letters.length >= 1
-  build_variant = letters.join('')
-
-  gradle(task: 'assemble' + build_variant + addition)
+  gradle(
+    tasks: tasks,
+    properties: properties
+  )
 end
