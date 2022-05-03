2de60280b386415b5afc8dcd0a6e5d13767d4217
Philip Lehmann-Boehm <philip@philiplb.de>
2022-05-03 17:28:23 +0200

    disabled more changelog

--- commons/smf_git_changelog/smf_read_changelog.rb
+++ commons/smf_git_changelog/smf_read_changelog.rb
@@ -1,20 +1,21 @@
 
 private_lane :smf_read_changelog do |options|
 
-  case options[:type]
-  when :html
-    UI.message("Reading changelog from #{_smf_changelog_html_temp_path}")
-    result = File.read(_smf_changelog_html_temp_path).to_s
-  when :slack_markdown
-    UI.message("Reading changelog from #{_smf_changelog_slack_markdown_temp_path}")
-    result = File.read(_smf_changelog_slack_markdown_temp_path).to_s
-  when :ticket_tags
-    UI.message("Reading ticket tags from #{_smf_ticket_tags_temp_path}")
-    result = File.read(_smf_ticket_tags_temp_path).to_s.split(' ')
-  else
-    UI.message("Reading changelog from #{_smf_changelog_temp_path}")
-    result =  File.read(_smf_changelog_temp_path).to_s
-  end
+  #case options[:type]
+  #when :html
+  #  UI.message("Reading changelog from #{_smf_changelog_html_temp_path}")
+  #  result = File.read(_smf_changelog_html_temp_path).to_s
+  #when :slack_markdown
+  #  UI.message("Reading changelog from #{_smf_changelog_slack_markdown_temp_path}")
+  #  result = File.read(_smf_changelog_slack_markdown_temp_path).to_s
+  #when :ticket_tags
+  #  UI.message("Reading ticket tags from #{_smf_ticket_tags_temp_path}")
+  #  result = File.read(_smf_ticket_tags_temp_path).to_s.split(' ')
+  #else
+  #  UI.message("Reading changelog from #{_smf_changelog_temp_path}")
+  #  result =  File.read(_smf_changelog_temp_path).to_s
+  #end
 
+  result = 'New Version'
   result
 end
