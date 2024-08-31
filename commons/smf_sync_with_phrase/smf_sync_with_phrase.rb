2290e27f2c291c03d68b5f03d5bfde0ac6d15751
Alexandru Taras <alex.taras@gmx.com>
2024-08-31 23:02:09 +0300

    add file format

--- commons/smf_sync_with_phrase/smf_sync_with_phrase.rb
+++ commons/smf_sync_with_phrase/smf_sync_with_phrase.rb
@@ -195,8 +195,16 @@ def _smf_upload_translation_file(api_client, project_id, locale_id, file, tags)
 
   begin
     UI.message("Uploading translation file: #{File.basename(file)}")
-    result = api_client.upload_create(project_id, File.new(file), locale_id, options)
-    UI.message("Updated #{File.basename(file)} at #{result.updated_at} UTC")
+    case @platform
+    when :ios, :macos, :apple
+      next unless item.end_with?(APPLE_LOCALIZABLE_FORMAT)
+      result = api_client.upload_create(project_id, File.new(file), APPLE_LOCALIZABLE_FORMAT, locale_id, options)
+      UI.message("Updated #{File.basename(file)} at #{result.updated_at} UTC")
+    when :android
+      next unless item.start_with?('strings')
+      result = api_client.upload_create(project_id, File.new(file), ANDROID_LOCALIZABLE_FORMAT, locale_id, options)
+      UI.message("Updated #{File.basename(file)} at #{result.updated_at} UTC")
+    end
   rescue Phrase::ApiError => e
     puts "Exception while uploading translation file #{file}: #{e}"
   end
