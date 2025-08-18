51fc2d0d07b26ae1e7a6040ccee48bffbb2d3a35
Rüdiger Weiß <rudivice@users.noreply.github.com>
2025-08-18 18:43:43 +0200

    new build path added (#256)

--- setup/android_setup.rb
+++ setup/android_setup.rb
@@ -717,7 +717,12 @@ def extract_package_from_build_gradle
     './app/build.gradle',
     './app/build.gradle.kts',
     './composeApp/build.gradle.kts',
-    './androidApp/build.gradle.kts'
+    './androidApp/build.gradle.kts',
+    # From fastlane directory perspective (go up one level)
+    '../app/build.gradle',
+    '../app/build.gradle.kts',
+    '../composeApp/build.gradle.kts',
+    '../androidApp/build.gradle.kts'
   ]
   
   build_gradle_paths.each do |path|
